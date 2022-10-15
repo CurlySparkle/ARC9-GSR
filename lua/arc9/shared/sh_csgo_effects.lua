3357ce8612e0c8d22ad96175cc0c11e559eff65c
May Lian <twilight_sparkle13@hotmail.com>
2022-10-15 17:20:14 -0300

    main

---
+++ lua/arc9/shared/sh_csgo_effects.lua
@@ -0,0 +1,11 @@
+game.AddParticles( "particles/cs_weapon_fx.pcf" )
+
+-- FX
+PrecacheParticleSystem("weapon_muzzle_flash_assaultrifle")
+PrecacheParticleSystem("weapon_muzzle_flash_autoshotgun")
+PrecacheParticleSystem("weapon_muzzle_flash_awp")
+PrecacheParticleSystem("weapon_muzzle_flash_huntingrifle")
+PrecacheParticleSystem("weapon_muzzle_flash_para")
+PrecacheParticleSystem("weapon_muzzle_flash_pistol")
+PrecacheParticleSystem("weapon_muzzle_flash_shotgun")
+PrecacheParticleSystem("weapon_muzzle_flash_smg")
---
+++ lua/arc9/shared/sh_csgo_sounds.lua
@@ -0,0 +1,38 @@
+sound.Add( {
+    name = "CSGO.m4a4.Fire",
+    channel = CHAN_WEAPON + 6,
+    volume = 1.0,
+    level = 140,
+    pitch = {95, 105},
+    sound = {
+        "^weapons/csgo/m4a1/m4a1_01.wav",
+		"^weapons/csgo/m4a1/m4a1_02.wav",
+		"^weapons/csgo/m4a1/m4a1_03.wav",
+		"^weapons/csgo/m4a1/m4a1_04.wav",
+    }
+} )
+
+sound.Add( {
+    name = "CSGO.m4a1.Fire_Unsil",
+    channel = CHAN_WEAPON + 6,
+    volume = 1.0,
+    level = 140,
+    pitch = {90, 115},
+    sound = {
+        "^weapons/csgo/m4a1/m4a1_silencer_01.wav",
+    }
+} )
+
+sound.Add( {
+    name = "CSGO.m4a1.Fire_Sil",
+    channel = CHAN_WEAPON + 6,
+    volume = 1.0,
+    level = 140,
+    pitch = {95, 105},
+    sound = {
+        "^weapons/csgo/m4a1/m4a1_us_01.wav",
+		"^weapons/csgo/m4a1/m4a1_us_02.wav",
+		"^weapons/csgo/m4a1/m4a1_us_03.wav",
+		"^weapons/csgo/m4a1/m4a1_us_04.wav",
+    }
+} )
