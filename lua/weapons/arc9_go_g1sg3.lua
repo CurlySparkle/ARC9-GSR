8579dd3b7d729f8c2d13da486c259f9030f0907e
May Lian <twilight_sparkle13@hotmail.com>
2022-10-22 01:39:59 -0300

    added g3sg1

--- lua/weapons/arc9_go_g1sg3.lua
+++ lua/weapons/arc9_go_g1sg3.lua
@@ -5,16 +5,15 @@ SWEP.Base = "arc9_base"
 SWEP.Spawnable = true
 SWEP.Category = "ARC-9 - CS:GO"
 
-SWEP.PrintName = "P2000"
-SWEP.TrueName = "P2000"
+SWEP.PrintName = "G3SG1"
 
-SWEP.Class = "Pistol"
+SWEP.Class = "Sniper Rifle"
 SWEP.Trivia = {
-    ["Country of Origin"] = "United States of America",
-    ["Caliber"] = "5.56 NATO",
+    ["Country of Origin"] = "Switzerland",
+    ["Caliber"] = "5.56×45 NATO",
     ["Weight (Loaded)"] = "3.22kg",
     ["Projectile Weight"] = "4 Grams",
-    ["Muzzle Velocity"] = "2900 Feet/Second",
+    ["Muzzle Velocity"] = "2,989 Feet/Second",
     ["Muzzle Energy"] = "1570 Joules"
 }
 
@@ -23,15 +22,15 @@ SWEP.Credits = {
     Assets = "Counter-Strike Global Offensive"
 }
 
-SWEP.Description = [[Accurate and controllable, the German-made P2000 is a serviceable first-round pistol that works best against unarmored opponents.]]
+SWEP.Description = [[The pricy G3SG1 lowers movement speed considerably but compensates with a higher rate of fire than other sniper rifles.]]
 
-SWEP.ViewModel = "models/weapons/csgo/c_pist_p2000.mdl"
-SWEP.WorldModel = "models/weapons/w_pist_usp.mdl"
+SWEP.ViewModel = "models/weapons/csgo/c_snip_g3sg1.mdl"
+SWEP.WorldModel = "models/weapons/w_snip_g3sg1.mdl"
 
-SWEP.Slot = 1
+SWEP.Slot = 3
 
 SWEP.MirrorVMWM = true
-SWEP.WorldModelMirror = "models/weapons/csgo/c_pist_p2000.mdl"
+SWEP.WorldModelMirror = "models/weapons/csgo/c_snip_g3sg1.mdl"
 SWEP.WorldModelOffset = {
     Pos = Vector(-15, 9, -7),
     Ang = Angle(-5, 0, 180),
@@ -40,28 +39,36 @@ SWEP.WorldModelOffset = {
 
 -------------------------- DAMAGE PROFILE
 
-SWEP.DamageMax = 35 -- Damage done at point blank range
-SWEP.DamageMin = 15 -- Damage done at maximum range
+SWEP.DamageMax = 80 -- Damage done at point blank range
+SWEP.DamageMin = 30 -- Damage done at maximum range
 
 SWEP.DamageRand = 0.1 -- Damage varies randomly per shot by this fraction. 0.1 = +- 10% damage per shot.
 
 SWEP.RangeMin = 2000 -- How far bullets retain their maximum damage for.
-SWEP.RangeMax = 8192 -- In Hammer units, how far bullets can travel before dealing DamageMin.
+SWEP.RangeMax = 15000 -- In Hammer units, how far bullets can travel before dealing DamageMin.
 
-SWEP.Penetration = 15 -- Units of wood that can be penetrated by this gun.
+SWEP.Penetration = 25 -- Units of wood that can be penetrated by this gun.
 
 SWEP.ImpactForce = 15
 
+SWEP.BodyDamageMults = {
+    [HITGROUP_HEAD] = 2,
+    [HITGROUP_CHEST] = 1,
+    [HITGROUP_LEFTARM] = 0.9,
+    [HITGROUP_RIGHTARM] = 0.9,
+}
+
+
 -------------------------- PHYS BULLET BALLISTICS
 
-SWEP.PhysBulletMuzzleVelocity = 2900 * 12
+SWEP.PhysBulletMuzzleVelocity = 2800 * 12
 
 -------------------------- MAGAZINE
 
-SWEP.Ammo = "pistol" -- What ammo type this gun uses.
+SWEP.Ammo = "ar2" -- What ammo type this gun uses.
 
 SWEP.ChamberSize = 1 -- The amount of rounds this gun can chamber.
-SWEP.ClipSize = 13 -- Self-explanatory.
+SWEP.ClipSize = 20 -- Self-explanatory.
 SWEP.SupplyLimit = 6 -- Amount of magazines of ammo this gun can take from an ARC9 supply crate.
 SWEP.SecondarySupplyLimit = 2 -- Amount of reserve UBGL magazines you can take.
 
@@ -71,7 +78,7 @@ SWEP.Crosshair = true
 
 -------------------------- FIREMODES
 
-SWEP.RPM = 352
+SWEP.RPM = 240
 
 SWEP.Firemodes = {
     {
@@ -82,12 +89,10 @@ SWEP.Firemodes = {
 -------------------------- RECOIL
 
 -- General recoil multiplier
-SWEP.Recoil = 0.9
-
-SWEP.RecoilSeed = 59299 -- CSGO Seed Input Test
+SWEP.Recoil = 1.3
 
 -- These multipliers affect the predictible recoil by making the pattern taller, shorter, wider, or thinner.
-SWEP.RecoilUp = 0.5 -- Multiplier for vertical recoil
+SWEP.RecoilUp = 0.7 -- Multiplier for vertical recoil
 SWEP.RecoilSide = 0.7 -- Multiplier for vertical recoil
 
 -- These values determine how much extra movement is applied to the recoil entirely randomly, like in a circle.
@@ -100,32 +105,53 @@ SWEP.RecoilResetTime = 0 -- How long the gun must go before the recoil pattern s
 
 SWEP.RecoilAutoControl = 1 -- Multiplier for automatic recoil control.
 
-SWEP.RecoilKick = 1
+SWEP.RecoilKick = 0.5
 
 SWEP.RecoilMultCrouch = 0.6
+
 SWEP.RecoilMultHipFire = 1.25
 SWEP.RecoilAutoControlMultHipFire = 0.5
 
+-------------------------- VISUAL RECOIL
+
+SWEP.RecoilKick = 1
+
+SWEP.UseVisualRecoil = true
+
+SWEP.VisualRecoilUp = 0 -- Vertical tilt for visual recoil.
+SWEP.VisualRecoilSide = 0.01 -- Horizontal tilt for visual recoil.
+SWEP.VisualRecoilRoll = 0 -- Roll tilt for visual recoil.
+
+SWEP.VisualRecoilCenter = Vector(0, 4, 0) -- The "axis" of visual recoil. Where your hand is.
+
+SWEP.VisualRecoilPunch = 1 -- How far back visual recoil moves the gun.
+
+SWEP.VisualRecoilMult = 1
+
+SWEP.VisualRecoilHipFire = false
+
 -------------------------- SPREAD
 
-SWEP.Spread = 0.02
+SWEP.Spread = 0.002
 
-SWEP.SpreadAddRecoil = 0.005 -- Applied per unit of recoil.
+SWEP.SpreadAddRecoil = 0.0002 -- Applied per unit of recoil.
 
-SWEP.SpreadAddMove = 0.07
-SWEP.SpreadAddMidAir = 0.03
-SWEP.SpreadAddHipFire = 0.02
-SWEP.SpreadAddCrouch = -0.05
+SWEP.SpreadAddMove = 0.1
+SWEP.SpreadAddMidAir = 0.1
+SWEP.SpreadAddHipFire = 0.1
+SWEP.SpreadAddCrouch = -0.04
 
 -------------------------- HANDLING
 
 SWEP.FreeAimRadius = 0 -- In degrees, how much this gun can free aim in hip fire.
 SWEP.Sway = 0 -- How much the gun sways.
 
-SWEP.SwayMultSights = 0.3
+SWEP.SwayMultSights = 0
+
+SWEP.SpeedMultSights = 0.65
 
 SWEP.AimDownSightsTime = 0.31 -- How long it takes to go from hip fire to aiming down sights.
-SWEP.SprintToFireTime = 0.2 -- How long it takes to go from sprinting to being able to fire.
+SWEP.SprintToFireTime = 0.3 -- How long it takes to go from sprinting to being able to fire.
 
 -------------------------- MELEE
 
@@ -141,25 +167,25 @@ SWEP.PostBashTime = 0.5
 -------------------------- TRACERS
 
 SWEP.TracerNum = 1 -- Tracer every X
-SWEP.TracerColor = Color(255, 255, 155) -- Color of tracers. Only works if tracer effect supports it. For physical bullets, this is compressed down to 9-bit color.
+SWEP.TracerColor = Color(255, 255, 200) -- Color of tracers. Only works if tracer effect supports it. For physical bullets, this is compressed down to 9-bit color.
 
 -------------------------- POSITIONS
 
 SWEP.IronSights = {
-    Pos = Vector(-2.83, -3, 0.75),
-    Ang = Angle(0, 1.2, 0),
-    Magnification = 1.25,
+    Pos = Vector(-5.16, -10, 1.15),
+    Ang = Angle(0.025, -0.2, 0),
+    Magnification = 2,
     ViewModelFOV = 56,
 }
 
 SWEP.ViewModelFOVBase = 56
 
-SWEP.SprintPos = Vector(-1, -1, 0)
-SWEP.SprintAng = Angle(-5, 0, 5)
+SWEP.SprintPos = Vector(0, -2, -0.3)
+SWEP.SprintAng = Angle(0, 0, 0)
 
 SWEP.SprintMidPoint = {
-    Pos = Vector(-0.5, -2.5, 0),
-    Ang = Angle(-2.5, 0, 2.5)
+    Pos = Vector(0, -1, -0.15),
+    Ang = Angle(0, 0, 0)
 }
 
 SWEP.ActivePos = Vector(0, 0, 0)
@@ -177,67 +203,58 @@ SWEP.CrouchPos = Vector(-0.5, -0, -1)
 SWEP.CrouchAng = Angle(0, 0, 0)
 
 SWEP.CustomizeAng = Angle(90, 0, 0)
-SWEP.CustomizePos = Vector(18, 20, 2.5)
+SWEP.CustomizePos = Vector(24, 32, 8)
 SWEP.CustomizeSnapshotFOV = 90
-SWEP.CustomizeSnapshotPos = Vector(0, -10, 2)
+SWEP.CustomizeSnapshotPos = Vector(-1, 7, 0)
 SWEP.CustomizeSnapshotAng = Angle(0, 0, 0)
 SWEP.CustomizeNoRotate = false
-
-SWEP.BlindFirePos = Vector(-3, -1, 2)
-SWEP.BlindFireAng = Angle(0, 0, -50)
-
-SWEP.BlindFireRightPos = Vector(-12, 25, 0)
-SWEP.BlindFireRightAng = Angle(-90, -20, 0)
-
-SWEP.BlindFireLeftPos = Vector(12, 25, 0)
-SWEP.BlindFireLeftAng = Angle(90, -20, 0)
 
 -------------------------- HoldTypes
 
 SWEP.HoldType = "rpg"
 SWEP.HoldTypeSprint = "normal"
 SWEP.HoldTypeHolstered = "normal"
-SWEP.HoldTypeSights = "ar2"
+SWEP.HoldTypeSights = "smg"
 SWEP.HoldTypeCustomize = "slam"
 SWEP.HoldTypeBlindfire = "pistol"
-SWEP.HoldTypeNPC = "pistol"
 
 SWEP.AnimShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
 SWEP.AnimReload = ACT_HL2MP_GESTURE_RELOAD_AR2
 SWEP.AnimDraw = false
+
 -------------------------- EFFECTS
 
-SWEP.MuzzleParticle = "weapon_muzzle_flash_pistol"
-SWEP.AfterShotParticle = "weapon_muzzle_smoke_pistols"
+SWEP.MuzzleParticle = "weapon_muzzle_flash_huntingrifle"
+SWEP.AfterShotParticle = "weapon_muzzle_smoke"
 SWEP.MuzzleEffectQCA = 1
 SWEP.ProceduralViewQCA = 1
 
 SWEP.CamOffsetAng = Angle(0, 0, 0)
 SWEP.NoViewBob = false
 
-SWEP.ShouldDropMag = true
-SWEP.ShouldDropMagEmpty = true
-
-SWEP.ShellModel = "models/models/weapons/shared/shell_9mm_hr.mdl"
+SWEP.ShellModel = "models/models/weapons/shared/shell_762_hr.mdl"
 SWEP.ShellCorrectAng = Angle(0, 0, 0)
 SWEP.ShellScale = 0.09
 SWEP.ShellPhysBox = Vector(0.5, 0.5, 2)
 
 SWEP.ShouldDropMag = true
-SWEP.DropMagazineModel = "models/weapons/csgo/mags/w_pist_p2000_mag.mdl"
+SWEP.DropMagazineModel = "models/weapons/csgo/mags/w_snip_g3sg1_mag.mdl" -- Set to a string or table to drop this magazine when reloading.
 SWEP.DropMagazineSounds = {"physics/metal/weapon_impact_soft1.wav", "physics/metal/weapon_impact_soft2.wav", "physics/metal/weapon_impact_soft3.wav"}
-SWEP.DropMagazineAmount = 1
+SWEP.DropMagazineAmount = 1 -- Amount of mags to drop.
 SWEP.DropMagazineTime = 0.35
 
 -------------------------- SOUNDS
 
-local path = "weapons/csgo/hkp2000/"
+local path = "weapons/csgo/g3sg1/"
 
-SWEP.ShootSound = "CSGO.hkp2000.Fire"
-SWEP.DistantShootSound = "CSGO.hkp2000.Fire.Distance"
+SWEP.ShootSound = "CSGO.g3sg1.Fire"
+SWEP.DistantShootSound = "CSGO.g3sg1.Distance_Fire"
 SWEP.DryFireSound = "weapons/clipempty_rifle.wav"
 
 SWEP.FiremodeSound = "arc9/firemode.wav"
+
+SWEP.EnterSightsSound = path .. "sg556_zoom_in.wav"
+SWEP.ExitSightsSound = path .. "sg556_zoom_out.wav"
 
 SWEP.HideBones = {
 }
@@ -257,26 +274,25 @@ SWEP.Animations = {
     ["reload"] = {
         Source = "reload_short",
         EventTable = {
-            {s = path .. "hkp2000_clipout.wav", t = 12 / 30},
-            {s = path .. "hkp2000_clipin.wav", t = 25 / 30},
+            {s = path .. "g3sg1_clipout.wav", t = 16 / 30},
+            {s = path .. "g3sg1_clipin.wav", t = 73 / 30},
         },
     },
     ["reload_empty"] = {
         Source = "reload",
-		MinProgress = 0.4,
         EventTable = {
-            {s = path .. "hkp2000_clipout.wav", t = 12 / 30},
-            {s = path .. "hkp2000_clipin.wav", t = 25 / 30},
-            {s = path .. "hkp2000_slideback.wav", t = 44 / 30},
-            {s = path .. "hkp2000_sliderelease.wav", t = 50 / 30},
+            {s = path .. "g3sg1_clipout.wav", t = 16 / 30},
+            {s = path .. "g3sg1_clipin.wav", t = 73 / 30},
+            {s = path .. "g3sg1_slideback.wav", t = 97 / 30},
+			{s = path .. "g3sg1_slideforward.wav", t = 104 / 30},
         },
     },
     ["ready"] = {
         Source = "draw",
         EventTable = {
-            {s = path .. "hkp2000_draw.wav", t = 0 / 30},
-            {s = path .. "hkp2000_slideback.wav", t = 8 / 30},
-            {s = path .. "hkp2000_sliderelease.wav", t = 14 / 30},
+            {s = path .. "g3sg1_draw.wav", t = 0 / 30},
+            {s = path .. "g3sg1_slideback.wav", t = 6 / 30},
+            {s = path .. "g3sg1_slideforward.wav", t = 12 / 30},
         },
     },
     ["draw"] = {
@@ -304,20 +320,51 @@ SWEP.Animations = {
     },
     ["inspect"] = {
         Source = "lookat01",
-        MinProgress = 0.1,
-        FireASAP = true,
         EventTable = {
-            { s = "weapons/csgo/movement1.wav", t = 2 / 30 },
-            { s = "weapons/csgo/movement2.wav", t = 175 / 30 },
+            { s = "weapons/csgo/movement1.wav", t = 5 / 30 },
+            { s = "weapons/csgo/movement2.wav", t = 74 / 30 },
+            { s = "weapons/csgo/movement3.wav", t = 111 / 30 },
         },
     },
 }
 
-SWEP.Hook_Think	= ARC9.CSGO.BlendEmpty
-
 -------------------------- ATTACHMENTS
 
+SWEP.DefaultBodygroups = "0000"
+
 SWEP.AttachmentElements = {
+    ["rearsight"] = {
+        Bodygroups = {
+            {1,1},
+            {2,1},
+        },
+    },
+    ["rail"] = {
+        Bodygroups = {
+            {1,1},
+            {2,1},
+        },
+    },
 }
 
-SWEP.Attachments = {}
+SWEP.Attachments = {
+    {
+        PrintName = "Scope",
+        Bone = "v_weapon.g3sg1_Parent",
+        Pos = Vector(0, -6.1, 3.4),
+        Ang = Angle(90, 0, -90),
+        Category = {"csgo_optic"},
+        InstalledElements = {"rearsight","rail"},
+        --Installed = "csgo_optic_scope_scar20",
+        CorrectiveAng = Angle(0, 0, 0),
+    },
+}
+
+
+SWEP.RTScope = true
+SWEP.RTScopeSubmatIndex = 2
+SWEP.RTScopeFOV = 10
+SWEP.RTScopeReticle = Material("hud/arc9_csgo/scopes/scope_overlay.png", "mips smooth")
+SWEP.RTScopeColorable = true
+SWEP.RTScopeShadowIntensity = 10
+SWEP.ScopeScreenRatio = 0.9
