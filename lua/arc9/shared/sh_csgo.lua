a2375b830ac85a41fb6d009852682d4c4466b834
May Lian <twilight_sparkle13@hotmail.com>
2022-10-28 23:18:31 -0300

    wutwut

--- lua/arc9/shared/sh_csgo.lua
+++ lua/arc9/shared/sh_csgo.lua
@@ -2,11 +2,13 @@ ARC9.CSGO = {}
 
 ARC9.CSGO.BlendEmpty = function(wep)
     local vm = wep:GetOwner():GetViewModel()
+    local delta = wep:GetSightDelta()
+    local smooth = math.cos(delta * (math.pi / 2))
     if wep:Clip1() == 0 --and !wep:GetReloading()-- This doesn't make it look well when reload due to the snappy instant pose set.
 	then
-        vm:SetPoseParameter("empty", 1)
+        vm:SetPoseParameter( "empty", Lerp(smooth, 0, 1) )
     else
-        vm:SetPoseParameter("empty", 0)
+        vm:SetPoseParameter( "empty", Lerp(smooth, 0, 0) )
     end
 end
 
@@ -14,45 +16,16 @@ ARC9.CSGO.BlendEmptyElite = function(wep)
     local vm = wep:GetOwner():GetViewModel()
     if wep:Clip1() == 1 then
         vm:SetPoseParameter("empty", 1)
-    else if wep:Clip1() == 0 then
+    elseif wep:Clip1() == 0 then
         vm:SetPoseParameter("empty", 2)
 	else
         vm:SetPoseParameter("empty", 0)
-     end
    end
 end
 
 ARC9.CSGO.BlendCylinder = function(wep)
     local vm = wep:GetOwner():GetViewModel()
-    vm:SetPoseParameter("cyl", wep:Clip1())
+    local deltap = math.cos(delta * (math.pi / 2))
+    vm:SetPoseParameter("cyl",8-wep:Clip1(),Lerp(deltap, 0, 1) )
 end
 
-ARC9.CSGO.BlendCylinder2 = function(wep)
-    local vm = wep:GetOwner():GetViewModel()
-    if wep:Clip1() == 7 then
-        vm:SetPoseParameter("cyl", 1)
-    else if wep:Clip1() == 6 then
-        vm:SetPoseParameter("cyl", 2)
-	else if wep:Clip1() == 5 then
-        vm:SetPoseParameter("cyl", 3)
-	else if wep:Clip1() == 4 then
-        vm:SetPoseParameter("cyl", 4)
-	else if wep:Clip1() == 3 then
-        vm:SetPoseParameter("cyl", 5)
-	else if wep:Clip1() == 2 then
-        vm:SetPoseParameter("cyl", 6)
-	else if wep:Clip1() == 1 then
-        vm:SetPoseParameter("cyl", 7)
-	else if wep:Clip1() == 0 then
-        vm:SetPoseParameter("cyl", 8)
-	else
-        vm:SetPoseParameter("cyl", 0)
-     end
-   end
-end
-end
-end
-end
-end
-end
-end
