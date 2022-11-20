00dc5c0a708a255d9b028e18b773dbe32576e51c
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2022-11-20 08:46:43 +0700

    mag drop

--- lua/weapons/arc9_go_aug.lua
+++ lua/weapons/arc9_go_aug.lua
@@ -218,7 +218,7 @@ SWEP.ShouldDropMag = true
 SWEP.DropMagazineModel = "models/weapons/csgo/mags/w_rif_aug_mag.mdl" -- Set to a string or table to drop this magazine when reloading.
 SWEP.DropMagazineSounds = {"physics/metal/weapon_impact_soft1.wav", "physics/metal/weapon_impact_soft2.wav", "physics/metal/weapon_impact_soft3.wav"}
 SWEP.DropMagazineAmount = 1 -- Amount of mags to drop.
-SWEP.DropMagazineTime = 0.65
+SWEP.DropMagazineTime = 0.45
 SWEP.DropMagazineQCA = 3
 
 -------------------------- SOUNDS
