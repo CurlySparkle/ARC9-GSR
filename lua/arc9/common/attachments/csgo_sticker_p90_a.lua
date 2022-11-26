3e16a2ef037e794d16d327671590ee890ade3635
Haodong Mo <mohaodong@gmail.com>
2022-11-26 14:44:21 +1000

    stickers only on p90 but you can make it for other guns too

--- lua/arc9/common/attachments/csgo_sticker_p90_a.lua
+++ lua/arc9/common/attachments/csgo_sticker_p90_a.lua
@@ -5,20 +5,7 @@ ATT.Icon = Material("stickers/necoarc.png", "mips smooth")
 ATT.SortOrder = 0
 ATT.Free = true
 
-ATT.Folder = "Side A" -- Change this to point what part of the weapon it goes.
+ATT.Folder = "Stickers" -- Change this to point what part of the weapon it goes.
 
-ATT.Category = {"stickers_p90"} -- Change this to point what weapon it is for.
-ATT.Model = "models/weapons/stickers/v_models/smg_p90_decal_a.mdl" -- Select the part of the weapon on where it goes, from A to D.
-ATT.ModelMaterial = "stickers/neco" -- Add a custom sticker image here.
-
-ATT.Attachments = {
-    {
-        PrintName = "Stickers",
-        DefaultCompactName = "Stickers",
-        Bone = "v_weapon.p90_Parent",
-        Pos = Vector(0, 0, 0),
-        Ang = Angle(0, 0, 0),
-        Icon_Offset = Vector(-2, 0, 0),
-        Category = "stickers_p90",
-    },
-}
+ATT.Category = {"stickers"} -- Change this to point what weapon it is for.
+ATT.StickerMaterial = "stickers/neco" -- Add a custom sticker image here.
