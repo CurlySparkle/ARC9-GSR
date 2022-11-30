be02fd6c9330f20365c48378603746679b89665b
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2022-11-30 21:25:10 +0700

    volkswangenitalia

---
+++ lua/weapons/arc9_go_akimbo_sawnoff.lua
@@ -0,0 +1,417 @@
+AddCSLuaFile()
+
+SWEP.Base = "arc9_go_base"
+
+SWEP.Spawnable = true
+SWEP.Category = "ARC-9 - CS:GO: Akimbo"
+
+SWEP.PrintName = "Dual Sawed-Off"
+SWEP.TrueName = "Dual Sawed-Off"
+
+SWEP.Class = "Shotgun"
+SWEP.Trivia = {
+    ["Country of Origin"] = "United States of America",
+    ["Caliber"] = "12×70 Shotshell",
+    ["Weight (Loaded)"] = "3.22kg",
+    ["Projectile Weight"] = "4 Grams",
+    ["Muzzle Velocity"] = "2900 Feet/Second",
+    ["Muzzle Energy"] = "1570 Joules"
+}
+
+SWEP.Credits = {
+    Author = "Arctic/Twilight",
+    Assets = "Counter-Strike Global Offensive"
+}
+
+SWEP.Description = [[The classic Sawed-Off deals very heavy close-range damage, but with its low accuracy, high spread and slow rate of fire, you'd better kill what you hit.]]
+
+SWEP.ViewModel = "models/weapons/csgo/c_akimbo_sawnoff.mdl"
+SWEP.WorldModel = "models/weapons/w_pist_elite.mdl"
+SWEP.DefaultBodygroups = "00000"
+
+SWEP.Slot = 1
+
+SWEP.MirrorVMWM = true
+SWEP.WorldModelMirror = "models/weapons/csgo/c_akimbo_sawnoff.mdl"
+SWEP.WorldModelOffset = {
+    Pos = Vector(-15, 9, -7),
+    Ang = Angle(-5, 0, 180),
+    Scale = 1,
+    TPIKPos = Vector(-12, 5, -14),
+    TPIKAng = Angle(0, -5, 190)
+}
+
+-------------------------- DAMAGE PROFILE
+
+SWEP.DamageMax = 36 -- Damage done at point blank range
+SWEP.DamageMin = 6 -- Damage done at maximum range
+
+SWEP.Num = 8
+
+SWEP.DamageRand = 0.1 -- Damage varies randomly per shot by this fraction. 0.1 = +- 10% damage per shot.
+
+SWEP.RangeMin = 750 -- How far bullets retain their maximum damage for.
+SWEP.RangeMax = 1750 -- In Hammer units, how far bullets can travel before dealing DamageMin.
+
+SWEP.Penetration = 10 -- Units of wood that can be penetrated by this gun.
+
+SWEP.ImpactForce = 15
+
+-------------------------- PHYS BULLET BALLISTICS
+
+SWEP.PhysBulletMuzzleVelocity = 1250 * 12
+
+SWEP.RicochetChance = 0
+
+-------------------------- MAGAZINE
+
+SWEP.Ammo = "buckshot" -- What ammo type this gun uses.
+
+SWEP.ChamberSize = 2 -- The amount of rounds this gun can chamber.
+SWEP.ClipSize = 8 -- Self-explanatory.
+SWEP.SupplyLimit = 6 -- Amount of magazines of ammo this gun can take from an ARC9 supply crate.
+SWEP.SecondarySupplyLimit = 2 -- Amount of reserve UBGL magazines you can take.
+
+SWEP.ReloadInSights = false -- This weapon can aim down sights while reloading.
+SWEP.DrawCrosshair = true
+SWEP.Crosshair = true
+
+-------------------------- FIREMODES
+
+SWEP.RPM = 80
+
+SWEP.Firemodes = {
+    {
+        Mode = 1,
+        PrintName = "PUMP"
+    },
+}
+-------------------------- RECOIL
+
+-- General recoil multiplier
+SWEP.Recoil = 1
+
+SWEP.RecoilSeed = 1089 -- CSGO Seed Input Test
+
+-- These multipliers affect the predictible recoil by making the pattern taller, shorter, wider, or thinner.
+SWEP.RecoilUp = 3 -- Multiplier for vertical recoil
+SWEP.RecoilSide = 1 -- Multiplier for vertical recoil
+
+-- These values determine how much extra movement is applied to the recoil entirely randomly, like in a circle.
+-- This type of recoil CANNOT be predicted.
+SWEP.RecoilRandomUp = 0.3
+SWEP.RecoilRandomSide = 0.3
+
+SWEP.RecoilDissipationRate = 40 -- How much recoil dissipates per second.
+SWEP.RecoilDissipationRateSights = 50
+SWEP.RecoilResetTime = 0 -- How long the gun must go before the recoil pattern starts to reset.
+
+SWEP.RecoilAutoControl = 3 -- Multiplier for automatic recoil control.
+
+SWEP.RecoilKick = 1.5
+
+SWEP.RecoilMultCrouch = 0.7
+SWEP.RecoilMultHipFire = 1.25
+SWEP.RecoilAutoControlMultHipFire = 0.5
+
+SWEP.ViewRecoil = true
+SWEP.ViewRecoilUpMult = 7
+
+SWEP.UseVisualRecoil = true
+SWEP.VisualRecoil = 2
+SWEP.VisualRecoilMultHipFire = 1
+SWEP.VisualRecoilMultSights = 1
+
+SWEP.Akimbo = true
+
+-------------------------- SPREAD
+
+SWEP.Spread = 0.05
+
+SWEP.SpreadAddRecoil = 0.0002 -- Applied per unit of recoil.
+
+SWEP.SpreadAddSights = 0
+SWEP.SpreadAddMove = 0.035
+SWEP.SpreadAddMidAir = 0.03
+SWEP.SpreadAddHipFire = 0.025
+SWEP.SpreadMultHipFire = 1
+SWEP.SpreadAddCrouch = -0.004
+SWEP.SpreadAddCrouch = -0.004
+SWEP.SpreadAddSightsMove = -0.1
+
+-------------------------- HANDLING
+
+SWEP.FreeAimRadius = 0 -- In degrees, how much this gun can free aim in hip fire.
+SWEP.Sway = 0 -- How much the gun sways.
+
+SWEP.SwayMultSights = 0.3
+
+SWEP.AimDownSightsTime = 0.31 -- How long it takes to go from hip fire to aiming down sights.
+SWEP.SprintToFireTime = 0.3 -- How long it takes to go from sprinting to being able to fire.
+
+-------------------------- MELEE
+
+SWEP.Bash = true
+SWEP.PrimaryBash = false
+
+SWEP.BashDamage = 50
+SWEP.BashLungeRange = 128
+SWEP.BashRange = 64
+SWEP.PreBashTime = 0.25
+SWEP.PostBashTime = 0.5
+
+-------------------------- TRACERS
+
+SWEP.TracerNum = 1 -- Tracer every X
+SWEP.TracerColor = Color(255, 255, 155) -- Color of tracers. Only works if tracer effect supports it. For physical bullets, this is compressed down to 9-bit color.
+
+-------------------------- POSITIONS
+
+-- SWEP.IronSights = {
+    -- Pos = Vector(0, 8, 0),
+    -- Ang = Angle(0, 0, 0),
+    -- Magnification = 1.15,
+    -- ViewModelFOV = 56,
+    -- CrosshairInSights = false
+-- }
+
+SWEP.IronSights = {
+    Pos = Vector(0, 3, 0.5),
+    Ang = Angle(0, 0, 2.5),
+    Midpoint = { -- Where the gun should be at the middle of it's irons
+        Pos = Vector(0, 0, -0),
+        Ang = Angle(0, 0, -2.5),
+    },
+    Magnification = 1.1,
+	ViewModelFOV = 56,
+    CrosshairInSights = true
+}
+
+SWEP.HasSights = true
+
+SWEP.ViewModelFOVBase = 56
+
+SWEP.SprintPos = Vector(0, 0, -1)
+SWEP.SprintAng = Angle(0, 0, 0)
+
+SWEP.SprintMidPoint = {
+    Pos = Vector(-0.5, -2.5, 0),
+    Ang = Angle(-2.5, 0, 2.5)
+}
+
+SWEP.ActivePos = Vector(-1, 0, 0)
+SWEP.ActiveAng = Angle(0, 0, 0)
+
+SWEP.MovingMidPoint = {
+    Pos = Vector(0, -0.5, -0.5),
+    Ang = Angle(0, 0, 0)
+}
+
+SWEP.MovingPos = Vector(0, -1, -1)
+SWEP.MovingAng = Angle(0, 0, 0)
+
+SWEP.CrouchPos = Vector(-0.5, -0, -1)
+SWEP.CrouchAng = Angle(0, 0, 0)
+
+SWEP.CustomizeAng = Angle(90, 0, 0)
+SWEP.CustomizePos = Vector(18, 30, 2.5)
+SWEP.CustomizeSnapshotFOV = 90
+SWEP.CustomizeSnapshotPos = Vector(3, -5, 3)
+SWEP.CustomizeSnapshotAng = Angle(0, 0, 0)
+SWEP.CustomizeNoRotate = false
+
+SWEP.BlindFirePos = Vector(-3, -1, 2)
+SWEP.BlindFireAng = Angle(0, 0, -50)
+
+SWEP.BlindFireRightPos = Vector(-12, 25, 0)
+SWEP.BlindFireRightAng = Angle(-90, -20, 0)
+
+SWEP.BlindFireLeftPos = Vector(12, 25, 0)
+SWEP.BlindFireLeftAng = Angle(90, -20, 0)
+
+-------------------------- HoldTypes
+
+SWEP.HoldType = "duel"
+SWEP.HoldTypeSprint = "duel"
+SWEP.HoldTypeHolstered = "duel"
+SWEP.HoldTypeSights = "duel"
+SWEP.HoldTypeCustomize = "slam"
+SWEP.HoldTypeBlindfire = "pistol"
+
+SWEP.AnimShoot = ACT_HL2MP_GESTURE_RELOAD_MAGIC
+SWEP.AnimReload = ACT_HL2MP_GESTURE_RELOAD_MAGIC
+SWEP.AnimDraw = false
+
+-------------------------- EFFECTS
+
+SWEP.MuzzleParticle = "weapon_muzzle_flash_autoshotgun"
+SWEP.AfterShotParticle = "weapon_muzzle_smoke"
+SWEP.MuzzleEffectQCA = 2
+SWEP.MuzzleEffectQCAEvenShot = 1
+
+SWEP.CaseEffectQCA = 4
+SWEP.CaseEffectQCAEvenShot = 3
+
+SWEP.AfterShotQCA = 1
+SWEP.AfterShotQCAEvenShot = 2
+
+SWEP.ProceduralViewQCA = 1
+
+SWEP.CamOffsetAng = Angle(0, 0, 0)
+SWEP.NoViewBob = false
+
+SWEP.ShellModel = "models/shells/shell_12gauge.mdl"
+SWEP.ShellCorrectAng = Angle(0, 0, 0)
+SWEP.ShellScale = 2
+SWEP.ShellPhysBox = Vector(0.5, 0.5, 2)
+
+SWEP.ShotgunReload = true
+
+-------------------------- SOUNDS
+
+local path = "weapons/csgo/sawedoff/"
+
+SWEP.ShootSound = "CSGO.sawedoff.Fire"
+SWEP.ShootSoundSilenced = "CSGO.sawedoff.Silenced_Fire"
+SWEP.DistantShootSound = "CSGO.sawedoff.Fire.Distance"
+SWEP.DryFireSound = "weapons/clipempty_rifle.wav"
+
+SWEP.FiremodeSound = "weapons/csgo/auto_semiauto_switch.wav"
+
+SWEP.HideBones = {
+}
+
+SWEP.HideBonesSilenced = {}
+
+SWEP.ReloadHideBoneTables = {
+}
+
+SWEP.Animations = {
+    ["fire_left"] = {
+        Source = "fire_left",
+        EventTable = {
+            {s = path .. "sawedoff_pump.wav", t = 15 / 30},			
+        },			
+    },
+    ["fire_right"] = {
+        Source = "fire1_right",
+        EventTable = {
+            {s = path .. "sawedoff_pump.wav", t = 15 / 30},			
+        },				
+    },
+    ["reload_start"] = {
+        Source = "reload_start",
+    },
+    ["reload_insert"] = {
+        Source = "reload",
+		Time = 0.75,
+        RestoreAmmo = 2,			
+        EventTable = {
+            {s = "CSGO.sawedoff.Shell_Insert", t = 2 / 30},
+            {s = "CSGO.sawedoff.Shell_Insert", t = 9 / 30},			
+        },
+    },
+    ["reload_finish"] = {
+        Source = "reload_end",
+        EventTable = {
+            {s = path .. "sawedoff_pump.wav", t = 7 / 30},	
+            {s = path .. "sawedoff_pump.wav", t = 10 / 30},				
+        },			
+    },
+    ["ready"] = {
+        Source = "reload_end",
+        EventTable = {
+            {s = path .. "sawedoff_pump.wav", t = 7 / 30},	
+            {s = path .. "sawedoff_pump.wav", t = 10 / 30},				
+        },	
+    },
+    ["draw"] = {
+        Source = "reload_end",
+        EventTable = {
+            {s = path .. "sawedoff_pump.wav", t = 7 / 30},	
+            {s = path .. "sawedoff_pump.wav", t = 10 / 30},				
+        },			
+    },
+    ["holster"] = {
+        Source = "idle",
+		Time = 0,
+    },
+    ["idle"] = {
+        Source = "idle",
+    },
+}
+
+-------------------------- ATTACHMENTS
+
+SWEP.AttachmentElements = {
+    ["mag"] = {
+        Bodygroups = {
+            {1,1},
+        },
+    },
+    ["slide_long"] = {
+        Bodygroups = {
+            {2,1},
+        },
+    AttPosMods = { [2] = { Pos = Vector(6, 0, 1), } }	
+    },
+}
+
+SWEP.Attachments = {
+    {
+        PrintName = "Slide",
+		--Bone = "v_weapon.glock_magazine",
+        Category = "go_glock_s"
+    },
+    {
+        PrintName = "Muzzle",
+        DefaultAttName = "Standard Muzzle",
+        Category = {"muzzle","muzzle_pistols"},
+        Bone = "tag_pistol_attachments_l",
+        Pos = Vector(5.23, 0, 1),
+        Ang = Angle(0, 0, 0),
+        DuplicateModels = {
+            {
+                Bone = "tag_pistol_attachments",
+            }
+        },
+        Scale = 0.7,
+    },
+    {
+        PrintName = "Sights",
+        Bone = "j_slide",
+        Pos = Vector(-2.5, 0, 0.2),
+        Ang = Angle(0, 0, 0),
+        Category = {"csgo_optics_pistols",},
+        CorrectiveAng = Angle(0.03, 0.1, 0),
+        DuplicateModels = {
+            {
+                Bone = "j_slide_l",
+            }
+        },
+		Scale = 0.9,
+    },
+    {
+        PrintName = "Tactical",
+        DefaultAttName = "Default",
+        Category = {"csgo_tac","csgo_tac_pistols"},
+        Bone = "tag_pistol_attachments",
+        Pos = Vector(4.3, 0, 0),
+        Ang = Angle(0, 0, 0),
+        DuplicateModels = {
+            {
+                Bone = "tag_pistol_attachments_l",
+            }
+        },
+		Scale = 1,
+    },
+    {
+        PrintName = "Mag",
+		Bone = "j_mag1_l",
+        Category = "go_mag"
+    },
+    {
+        PrintName = "Perk",
+        Category = "go_perk",
+    },
+}
