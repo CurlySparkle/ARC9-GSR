510efbfffe1d9752dc11586511cec278fd30c6ba
May Lian <twilight_sparkle13@hotmail.com>
2022-12-04 02:19:25 -0300

    added riot shield

--- lua/arc9/shared/sh_csgo_sounds_melee.lua
+++ lua/arc9/shared/sh_csgo_sounds_melee.lua
@@ -51,3 +51,44 @@ sound.Add( {
 		"^weapons/csgo/knifes/ct/knife_hit4.wav",
     }
 } )
+
+sound.Add( {
+    name = "CSGO.Shield.HitWall",
+    channel = CHAN_WEAPON,
+    volume = 1.0,
+    level = 125,
+    pitch = {95, 115},
+    sound = {
+        "^melee/shield_hit_world_01.wav",
+		"^melee/shield_hit_world_02.wav",
+		"^melee/shield_hit_world_03.wav",
+		"^melee/shield_hit_world_04.wav",
+    }
+} )
+
+sound.Add( {
+    name = "CSGO.Shield.HitBody",
+    channel = CHAN_WEAPON,
+    volume = 1.0,
+    level = 125,
+    pitch = {95, 115},
+    sound = {
+        "^melee/shield_hit_01.wav",
+		"^melee/shield_hit_02.wav",
+		"^melee/shield_hit_03.wav",
+    }
+} )
+
+sound.Add( {
+    name = "CSGO.Shield.Swing",
+    channel = CHAN_WEAPON,
+    volume = 1.0,
+    level = 125,
+    pitch = {95, 115},
+    sound = {
+        "^melee/shield_swing_01.wav",
+		"^melee/shield_swing_02.wav",
+		"^melee/shield_swing_03.wav",
+		"^melee/shield_swing_04.wav",
+    }
+} )
--- lua/weapons/arc9_go_knife_shield.lua
+++ lua/weapons/arc9_go_knife_shield.lua
@@ -5,34 +5,35 @@ SWEP.Base = "arc9_go_base"
 SWEP.Spawnable = true
 SWEP.Category = "ARC9 - CS:GO: Melee"
 
-SWEP.PrintName = "CT Knife"
+SWEP.PrintName = "Riot Shield"
 
 SWEP.Class = "Melee"
 
 SWEP.Credits = {
-    Author = "Twilight Sparle/eckololt",
+    Author = "Twilight Sparle/SlogoKolt",
     Assets = "Counter-Strike Global Offensive"
 }
 
-SWEP.Description = [[Quiet and always available, the knife has a fast, low-damage primary attack and a slow, high-damage secondary attack. A backstab will earn quiet players an instant kill.]]
+SWEP.Description = [[
+A protection device designed to deflect or absorb ballistic damage and help protect the carrier from an array of projectile calibers.
+]]
 
-SWEP.ViewModel = "models/weapons/csgo/knifes/v_knife_default_ct.mdl"
-SWEP.WorldModel = "models/weapons/csgo/knifes/v_knife_default_ct.mdl"
+SWEP.ViewModel = "models/weapons/csgo/v_shield.mdl"
+SWEP.WorldModel = "models/weapons/csgo/v_shield.mdl"
 
 SWEP.Slot = 0
 
 SWEP.MirrorVMWM = true
 SWEP.NoTPIKVMPos = true
-SWEP.WorldModelMirror = "models/weapons/csgo/knifes/v_knife_default_ct.mdl"
+SWEP.NoTPIK = true
+SWEP.WorldModelMirror = "models/weapons/csgo/v_shield.mdl"
 SWEP.WorldModelOffset = {
-    Pos = Vector(-13, 5, -3),
-    Ang = Angle(0, 0, 180),
+    Pos = Vector(-3, 6, -15),
+    Ang = Angle(10, 0, 185),
     Scale = 1
 }
 
 SWEP.DefaultBodygroups = "00"
-
-SWEP.CanBlindFire = false
 SWEP.DrawCrosshair = true
 SWEP.Crosshair = true
 -------------------------- MAGAZINE
@@ -67,19 +68,27 @@ SWEP.ShootWhileSprint = true
 SWEP.Bash = true
 SWEP.PrimaryBash = true
 
-SWEP.BashDamage = 70
+SWEP.BashDamage = 55
 SWEP.BashLungeRange = 0
 SWEP.BashRange = 64
-SWEP.PreBashTime = 0.2
-SWEP.PostBashTime = 0.2
+SWEP.PreBashTime = 0.35
+SWEP.PostBashTime = 0.35
 
 SWEP.ImpactForce = 15
 
-SWEP.MeleeHitSound = "CSGO.Knife.HitBody"
-SWEP.MeleeHitWallSound = "CSGO.Knife.HitWall"
-SWEP.MeleeSwingSound = "CSGO.Knife.Slash"
+SWEP.MeleeHitSound = "CSGO.Shield.HitBody"
+SWEP.MeleeHitWallSound = "CSGO.Shield.HitWall"
+SWEP.MeleeSwingSound = "CSGO.Shield.Swing"
 
 SWEP.FiremodeSound = ""
+
+-------------------------- SHIELD
+
+SWEP.ShieldModel = "models/weapons/csgo/w_shield_hitbox.mdl"
+SWEP.ShieldOffset = Vector(-3, 6, -15)
+SWEP.ShieldAngle = Angle(0, 15, 180)
+SWEP.ShieldScale = 1
+
 
 -------------------------- TRACERS
 
@@ -90,12 +99,12 @@ SWEP.TracerColor = Color(255, 225, 200) -- Color of tracers. Only works if trace
 
 SWEP.HasSights = false
 
-SWEP.SprintPos = Vector(0, 1, 0)
+SWEP.SprintPos = Vector(0, 0, 0)
 SWEP.SprintAng = Angle(0, 0, 0)
 
-SWEP.ViewModelFOVBase = 56
+SWEP.ViewModelFOVBase = 65
 
-SWEP.ActivePos = Vector(0, -2, 0)
+SWEP.ActivePos = Vector(0, 3, 0)
 SWEP.ActiveAng = Angle(0, 0, 0)
 
 SWEP.MovingMidPoint = {
@@ -114,9 +123,9 @@ SWEP.CrouchPos = Vector(-0.5, -0, -1)
 SWEP.CrouchAng = Angle(0, 0, 0)
 -------------------------- HoldTypes
 
-SWEP.HoldType = "rpg"
-SWEP.HoldTypeSprint = "rpg"
-SWEP.HoldTypeHolstered = "rpg"
+SWEP.HoldType = "duel"
+SWEP.HoldTypeSprint = "duel"
+SWEP.HoldTypeHolstered = "duel"
 SWEP.HoldTypeCustomize = "slam"
 SWEP.HoldTypeBlindfire = "pistol"
 
@@ -134,21 +143,12 @@ SWEP.Animations = {
         Source = "draw",
     },
     ["holster"] = {
-        Source = "draw",
-		Reverse = true,
-		Mult = 0.2,
+        Source = "holster",
     },
     ["bash"] = {
-        Source = {"light_hit1","light_hit2"}
+        Source = {"bash2","bash3"},
+		MinProgress = 0.7,
     },
-
-    ["bash_backstab"] = {
-        Source = {"light_backstab"},
-    },
-    ["bash2_backstab"] = {
-        Source = {"heavy_backstab"},
-    },
-
     ["idle_sprint"] = {
         Source = "sprint",
     },
@@ -162,28 +162,6 @@ SWEP.Animations = {
     },
     ["inspect"] = {
         Source = "lookat01",
-        IKTimeLine = {
-            {
-                t = 0,
-                lhik = 1,
-                rhik = 0
-            },
-            {
-                t = 0.2,
-                lhik = 0,
-                rhik = 0
-            },
-            {
-                t = 0.7,
-                lhik = 0,
-                rhik = 0
-            },
-            {
-                t = 0.9,
-                lhik = 1,
-                rhik = 1
-            },
-        },
         EventTable = {
             { s = "weapons/csgo/movement1.wav", t = 2 / 30 },
             { s = "weapons/csgo/movement2.wav", t = 92 / 30 },
