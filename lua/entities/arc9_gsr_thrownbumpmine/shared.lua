3b2d6c4c829e80af7c9aa6d5df865693e4256392
TheOnly8Z <888888zombies@gmail.com>
2022-12-17 17:38:36 -0600

    testing nade baes

--- lua/entities/arc9_gsr_thrownbumpmine/shared.lua
+++ lua/entities/arc9_gsr_thrownbumpmine/shared.lua
@@ -1,305 +1,314 @@
 -- Made by Matsilagi
-
 AddCSLuaFile()
-
 ENT.Type = "anim"
 ENT.Base = "base_entity"
-
 ENT.PrintName = "Bump Mine"
 ENT.Author = ""
 ENT.Category = "Globaly Offensive"
-
 ENT.Spawnable = false
 ENT.AdminSpawnable = false
 
 if CLIENT then
-	function ENT:Initialize()
-		self.ParticleCreated = false
-		self.ExplosionParticleCreated = false
-		self.PlayerParticleCreated = false
-		self.EmittedSound = false
-		self.Fused = false
-		if self:GetNWBool("Stuck") == nil then
-			self:SetNWBool("Stuck",false)
-		end
-	end
+    function ENT:Initialize()
+        self.ParticleCreated = false
+        self.ExplosionParticleCreated = false
+        self.PlayerParticleCreated = false
+        self.EmittedSound = false
+        self.Fused = false
 
-	function ENT:Draw()
-		self.Entity:DrawModel()
-				
-		--Particles	
-		local attachment = self:LookupAttachment("glow")
-		local attachment2 = self:LookupAttachment("glow_mid")
-		local data = self:GetAttachment(attachment)
-		local data2 = self:GetAttachment(attachment2)
-		local attachment_pos,attachment_ang,attachment2_pos,attachment2_ang
-		attachment_pos = data.Pos
-		attachment_ang = data.Ang
-		attachment2_pos = data2.Pos
-		attachment2_ang = data2.Ang
-		
-		local randomnum = math.random(1,2)
-		local randomnum2 = math.random(1,2)
-		local randomnum3 = math.random(1,2)
-		local randomnum4 = math.random(1,2)
-		local randomnum5 = math.random(1,2)
-		
-		local IsStuck = self:GetNWBool("Stuck")
-		
-		if self.NextRing == nil or self.NextRing < CurTime() then
-			if IsStuck != false then
-				self.NextRing = CurTime() + 0.02
-				local emitter = ParticleEmitter( attachment_pos, true )
+        if self:GetNWBool("Stuck") == nil then
+            self:SetNWBool("Stuck", false)
+        end
+    end
 
-				local particle = emitter:Add( "particle/particle_ring_wave_12", attachment_pos )
-				if ( !particle ) then return end
+    function ENT:Draw()
+        self:DrawModel()
+        --Particles
+        local attachment = self:LookupAttachment("glow")
+        local attachment2 = self:LookupAttachment("glow_mid")
+        local data = self:GetAttachment(attachment)
+        local data2 = self:GetAttachment(attachment2)
+        local attachment_pos, attachment_ang, attachment2_pos, attachment2_ang
+        attachment_pos = data.Pos
+        attachment_ang = data.Ang
+        attachment2_pos = data2.Pos
+        attachment2_ang = data2.Ang
+        local randomnum = math.random(1, 2)
+        local randomnum2 = math.random(1, 2)
+        local randomnum3 = math.random(1, 2)
+        local randomnum4 = math.random(1, 2)
+        local randomnum5 = math.random(1, 2)
 
-				particle:SetVelocity( self:GetUp() * 75 )
-				particle:SetLifeTime( 0 )
-				particle:SetDieTime( 0.2 )
-				if (randomnum2 == 1) then
-					particle:SetStartAlpha( 240 )
-				else
-					particle:SetStartAlpha( 250 )
-				end
-				if (randomnum3 == 1) then
-					particle:SetEndAlpha( 240 )
-				else
-					particle:SetEndAlpha( 250 )
-				end
-				particle:SetStartSize( 9.5 )
-				particle:SetEndSize( 9.5 )
-				particle:SetAngles( self:GetUp():Angle() )
-				particle:SetAngleVelocity( Angle(0,0,math.random(0,360)))
-				if (randomnum == 1) then
-					particle:SetColor(25, 90, 255)
-				else
-					particle:SetColor(28, 122, 251)
-				end
+        if (self.NextRing == nil or self.NextRing < CurTime()) and self:GetNWBool("Stuck") then
+            self.NextRing = CurTime() + 0.02
+            local emitter = ParticleEmitter(attachment_pos, true)
+            local particle = emitter:Add("particle/particle_ring_wave_12", attachment_pos)
+            if not particle then return end
+            particle:SetVelocity(self:GetUp() * 75)
+            particle:SetLifeTime(0)
+            particle:SetDieTime(0.2)
 
-				emitter:Finish()
-				
-				--MIDDLE RING
-				local emitter2 = ParticleEmitter( attachment2_pos, true )
-				local particle2 = emitter2:Add( "particle/particle_ring_wave_12", attachment2_pos )
-				if ( !particle2 ) then return end
+            if randomnum2 == 1 then
+                particle:SetStartAlpha(240)
+            else
+                particle:SetStartAlpha(250)
+            end
 
-				particle2:SetVelocity( self:GetUp() * 65 )
-				particle2:SetLifeTime( 0 )
-				particle2:SetDieTime( 0.2 )
-				if (randomnum4 == 1) then
-					particle2:SetStartAlpha( 220 )
-				else
-					particle2:SetStartAlpha( 250 )
-				end
-				if (randomnum5 == 1) then
-					particle2:SetEndAlpha( 220 )
-				else
-					particle2:SetEndAlpha( 250 )
-				end
-				particle2:SetStartSize( 6 )
-				particle2:SetEndSize( 6 )
-				particle2:SetAngles( self:GetUp():Angle() )
-				particle2:SetAngleVelocity( Angle(0,0,math.random(0,360)))
-				if (randomnum == 1) then
-					particle2:SetColor(20, 86, 254)
-				else
-					particle2:SetColor(6, 0, 255)
-				end
+            if randomnum3 == 1 then
+                particle:SetEndAlpha(240)
+            else
+                particle:SetEndAlpha(250)
+            end
 
-				emitter2:Finish()
-			end
-		end
-	end
+            particle:SetStartSize(9.5)
+            particle:SetEndSize(9.5)
+            particle:SetAngles(self:GetUp():Angle())
+            particle:SetAngleVelocity(Angle(0, 0, math.random(0, 360)))
 
-	function ENT:DrawTranslucent()
-		self:Draw()
-	end
+            if randomnum == 1 then
+                particle:SetColor(25, 90, 255)
+            else
+                particle:SetColor(28, 122, 251)
+            end
+
+            emitter:Finish()
+            --MIDDLE RING
+            local emitter2 = ParticleEmitter(attachment2_pos, true)
+            local particle2 = emitter2:Add("particle/particle_ring_wave_12", attachment2_pos)
+            if not particle2 then return end
+            particle2:SetVelocity(self:GetUp() * 65)
+            particle2:SetLifeTime(0)
+            particle2:SetDieTime(0.2)
+
+            if randomnum4 == 1 then
+                particle2:SetStartAlpha(220)
+            else
+                particle2:SetStartAlpha(250)
+            end
+
+            if randomnum5 == 1 then
+                particle2:SetEndAlpha(220)
+            else
+                particle2:SetEndAlpha(250)
+            end
+
+            particle2:SetStartSize(6)
+            particle2:SetEndSize(6)
+            particle2:SetAngles(self:GetUp():Angle())
+            particle2:SetAngleVelocity(Angle(0, 0, math.random(0, 360)))
+
+            if randomnum == 1 then
+                particle2:SetColor(20, 86, 254)
+            else
+                particle2:SetColor(6, 0, 255)
+            end
+
+            emitter2:Finish()
+        end
+    end
+
+    function ENT:DrawTranslucent()
+        self:Draw()
+    end
 end
 
 if SERVER then
-	
-	function ENT:Initialize()
-		self:SetModel( "models/weapons/w_eq_bumpmine_dropped.mdl" )
-		self.ParticleCreated = false
-		self.ExplosionParticleCreated = false
-		self.EmittedSound = false
-		self.Fused = false
-		if self:GetNWBool("Stuck") == nil then
-			self:SetNWBool("Stuck",false)
-		end
-		
-		self:PhysicsInit( SOLID_VPHYSICS )
-		self:SetMoveType( MOVETYPE_VPHYSICS )
+    function ENT:Initialize()
+        self:SetModel("models/weapons/w_eq_bumpmine_dropped.mdl")
+        self.ParticleCreated = false
+        self.ExplosionParticleCreated = false
+        self.EmittedSound = false
+        self.Fused = false
 
-		local phys = self:GetPhysicsObject()
-		phys:Wake()
+        if self:GetNWBool("Stuck") == nil then
+            self:SetNWBool("Stuck", false)
+        end
 
-		self:SetCollisionGroup(COLLISION_GROUP_DEBRIS_TRIGGER)
+        self:PhysicsInit(SOLID_VPHYSICS)
+        self:SetMoveType(MOVETYPE_VPHYSICS)
+        local phys = self:GetPhysicsObject()
+        phys:Wake()
+        self:SetCollisionGroup(COLLISION_GROUP_DEBRIS_TRIGGER)
+        self.TimesUsed = 1
+    end
 
-		self.TimesUsed = 1
-	end
-	
-	function ENT:OnFloor()
-		local p = math.Round(self:GetAngles().p)
-		return p == 270 or p == -90
-	end
-	
-	function ENT:PhysicsCollide( data, phys )
-		local ent = data.HitEntity
-		local phys = self:GetPhysicsObject()
+    function ENT:OnFloor()
+        local p = math.Round(self:GetAngles().p)
 
-		if ent:IsNPC() or ent:IsPlayer() or ent:IsNextBot() then return false end
-		if !self:IsValid() then return end
+        return p == 270 or p == -90
+    end
 
-		if self:GetNWBool("Stuck") != true then
-			self:SetNWBool("Stuck",true)
-		end
-			
-		self:EmitSound("CSGO.Mine.Armed")
-		self:SetBodygroup(1,1)
-		
-		if ent:IsWorld() then
-			phys:EnableMotion(false)
-			orient_angles(phys,data)
-		else
-			constraint.Weld(self, ent, 0, 0, 0, true, true)
-			orient_angles(phys,data)
-		end
-		
-		if self.ParticleCreated != true then
-			local pos = self:GetPos()
-			local ang = self:GetAngles()
-			
-			if self:OnFloor() then
-				pos = pos-Vector(0,0,20)
-			end
-			
-			local attach = self:LookupAttachment("glow_start")
-			local lbone = self:LookupBone("glow")
-			local bone = self:GetBonePosition(lbone)
-			local data = self:GetAttachment(attach)
-			local attpos,attang
-			attpos = data.Pos
-			attang = data.Ang
-			ParticleEffectAttach("bumpmine_active_glow2",PATTACH_POINT_FOLLOW,self,attach)
-			--ParticleEffect("bumpmine_active",bone,attang,self)
-			
-			self.idlesound = CreateSound(self, "CSGO.Mine.Idle")
-			self.idlesound:Play()
-			self.ParticleCreated = true
-		end
-		
-	end
+    function ENT:PhysicsCollide(data, phys)
+        local ent = data.HitEntity
+        local phys = self:GetPhysicsObject()
+        if ent:IsNPC() or ent:IsPlayer() or ent:IsNextBot() then return false end
+        if not self:IsValid() then return end
 
-	function ENT:Think()
-		local IsStuck = self:GetNWBool("Stuck")
-		if IsStuck != true then return false end
-		
-		if self.Fused != false then
-			self:Destroy()
-		end
-		
-		local entsph = ents.FindInSphere(self:GetPos(), 25)
-		for k, v in pairs(entsph) do
-			if IsValid(v) and v:IsPlayer() or v:IsNPC() or v:IsNextBot() then
-				if self.EmittedSound != true then
-					self:EmitSound("CSGO.Mine.PreDetonate")
-					self.EmittedSound = true
-				end
-				timer.Simple(0.12,function()
-					self.Fused = true
-				end)
-			end
-		end
-	end
+        if self:GetNWBool("Stuck") ~= true then
+            self:SetNWBool("Stuck", true)
+        end
 
-	function ENT:Destroy()
-		self:EmitSound("CSGO.Mine.Detonate")
-		self:SetNoDraw(true)	
-		
-		local ownersearch = ents.FindInSphere(self:GetPos(), 100)
-		for k, v in pairs(ownersearch) do
-			if IsValid(v) and v:IsPlayer() then
-				if self:GetOwner() == v then
-					print("OwnerShake")
-					util.ScreenShake( self:GetPos(), 25.0, 150.0, 1.0, 750 )
-				end
-			end
-		end
-		
-		local entsph2 = ents.FindInSphere(self:GetPos(), 100)
-		for k, v in pairs(entsph2) do
-			if IsValid(v) and v:IsPlayer() or v:IsNPC() or v:IsNextBot() then
-				v:SetVelocity(self:GetAngles():Up()*1000)
-				if self.PlayerParticleCreated != true then
-					local trail = ents.Create("info_particle_system")
-					trail:SetKeyValue("effect_name","bumpmine_player_trail")
-					trail:SetPos( self:GetPos() )
-					trail:SetAngles( self:GetAngles() )
-					trail:SetParent( v )
-					trail:Spawn()
-					trail:Activate()
-					trail:Fire("Start","",0)
-					trail:Fire("Kill","",8)
-					self.PlayerParticleCreated = true
-				end
-			end
-		end
-		
-		if self.ExplosionParticleCreated != true then
-			local explo = ents.Create("info_particle_system")
-			explo:SetKeyValue("effect_name","bumpmine_detonate")
-			explo:SetPos( self:GetPos() )
-			explo:SetAngles( self:GetAngles() )
-			explo:SetParent( self )
-			explo:Spawn()
-			explo:Activate()
-			explo:Fire("Start","",0)
-			explo:Fire("Kill","",8)
-			self.ExplosionParticleCreated = true
-		end
-		
-		timer.Simple(0.001, function()
-			if(self and self:IsValid()) then
-				self:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
-			end
-		end)
-		timer.Simple(0.002, function()
-			if(self and self:IsValid()) then
-				self:Remove()
-			end
-		end)
-	end
-	
-	function ENT:OnRemove()
-		if self.idlesound then self.idlesound:Stop() end
-	end
-	
-	function orient_angles(obj, data) --this function juts takes in the hitnormal of the collision and rotates the angles accordingly
-		if data.HitNormal.z < -.5 then
-			obj:SetAngles((data.HitNormal + Vector(0,90,0) ):Angle())
-			return 
-		end
-		if data.HitNormal.z > .5 then
-			obj:SetAngles((data.HitNormal + Vector(90,0,0) ):Angle())
-			return 
-		end
-		if data.HitNormal.y < -.5 then
-			obj:SetAngles((data.HitNormal + Vector(0,0,90) ):Angle())
-			return 
-		end
-		if data.HitNormal.y > .5 then
-			obj:SetAngles((data.HitNormal + Vector(0,0,90) ):Angle())
-			return 
-		end
-		if data.HitNormal.x < -.5 then
-			obj:SetAngles((data.HitNormal + Vector(0,0,90) ):Angle())
-			return 
-		end
-		if data.HitNormal.x > .5 then
-			obj:SetAngles((data.HitNormal + Vector(0,0,90) ):Angle())
-			return 
-		end
-	end
+        self:EmitSound("CSGO.Mine.Armed")
+        self:SetBodygroup(1, 1)
+
+        if ent:IsWorld() then
+            phys:EnableMotion(false)
+            orient_angles(phys, data)
+        else
+            constraint.Weld(self, ent, 0, 0, 0, true, true)
+            orient_angles(phys, data)
+        end
+
+        if self.ParticleCreated ~= true then
+            local pos = self:GetPos()
+            local ang = self:GetAngles()
+
+            if self:OnFloor() then
+                pos = pos - Vector(0, 0, 20)
+            end
+
+            local attach = self:LookupAttachment("glow_start")
+            local lbone = self:LookupBone("glow")
+            local bone = self:GetBonePosition(lbone)
+            local data = self:GetAttachment(attach)
+            local attpos, attang
+            attpos = data.Pos
+            attang = data.Ang
+            ParticleEffectAttach("bumpmine_active_glow2", PATTACH_POINT_FOLLOW, self, attach)
+            --ParticleEffect("bumpmine_active",bone,attang,self)
+            self.idlesound = CreateSound(self, "CSGO.Mine.Idle")
+            self.idlesound:Play()
+            self.ParticleCreated = true
+        end
+    end
+
+    function ENT:Think()
+        local IsStuck = self:GetNWBool("Stuck")
+        if IsStuck ~= true then return false end
+
+        if self.Fused ~= false then
+            self:Destroy()
+        end
+
+        local entsph = ents.FindInSphere(self:GetPos(), 25)
+
+        for k, v in pairs(entsph) do
+            if IsValid(v) and v:IsPlayer() or v:IsNPC() or v:IsNextBot() then
+                if self.EmittedSound ~= true then
+                    self:EmitSound("CSGO.Mine.PreDetonate")
+                    self.EmittedSound = true
+                end
+
+                timer.Simple(0.12, function()
+                    self.Fused = true
+                end)
+            end
+        end
+    end
+
+    function ENT:Destroy()
+        self:EmitSound("CSGO.Mine.Detonate")
+        self:SetNoDraw(true)
+        local ownersearch = ents.FindInSphere(self:GetPos(), 100)
+
+        for k, v in pairs(ownersearch) do
+            if IsValid(v) and v:IsPlayer() then
+                if self:GetOwner() == v then
+                    print("OwnerShake")
+                    util.ScreenShake(self:GetPos(), 25.0, 150.0, 1.0, 750)
+                end
+            end
+        end
+
+        local entsph2 = ents.FindInSphere(self:GetPos(), 100)
+
+        for k, v in pairs(entsph2) do
+            if IsValid(v) and v:IsPlayer() or v:IsNPC() or v:IsNextBot() then
+                v:SetVelocity(self:GetAngles():Up() * 1000)
+
+                if self.PlayerParticleCreated ~= true then
+                    local trail = ents.Create("info_particle_system")
+                    trail:SetKeyValue("effect_name", "bumpmine_player_trail")
+                    trail:SetPos(self:GetPos())
+                    trail:SetAngles(self:GetAngles())
+                    trail:SetParent(v)
+                    trail:Spawn()
+                    trail:Activate()
+                    trail:Fire("Start", "", 0)
+                    trail:Fire("Kill", "", 8)
+                    self.PlayerParticleCreated = true
+                end
+            end
+        end
+
+        if self.ExplosionParticleCreated ~= true then
+            local explo = ents.Create("info_particle_system")
+            explo:SetKeyValue("effect_name", "bumpmine_detonate")
+            explo:SetPos(self:GetPos())
+            explo:SetAngles(self:GetAngles())
+            explo:SetParent(self)
+            explo:Spawn()
+            explo:Activate()
+            explo:Fire("Start", "", 0)
+            explo:Fire("Kill", "", 8)
+            self.ExplosionParticleCreated = true
+        end
+
+        timer.Simple(0.001, function()
+            if self and self:IsValid() then
+                self:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
+            end
+        end)
+
+        timer.Simple(0.002, function()
+            if self and self:IsValid() then
+                self:Remove()
+            end
+        end)
+    end
+
+    function ENT:OnRemove()
+        if self.idlesound then
+            self.idlesound:Stop()
+        end
+    end
+
+    --this function juts takes in the hitnormal of the collision and rotates the angles accordingly
+    function orient_angles(obj, data)
+        if data.HitNormal.z < -.5 then
+            obj:SetAngles((data.HitNormal + Vector(0, 90, 0)):Angle())
+
+            return
+        end
+
+        if data.HitNormal.z > .5 then
+            obj:SetAngles((data.HitNormal + Vector(90, 0, 0)):Angle())
+
+            return
+        end
+
+        if data.HitNormal.y < -.5 then
+            obj:SetAngles((data.HitNormal + Vector(0, 0, 90)):Angle())
+
+            return
+        end
+
+        if data.HitNormal.y > .5 then
+            obj:SetAngles((data.HitNormal + Vector(0, 0, 90)):Angle())
+
+            return
+        end
+
+        if data.HitNormal.x < -.5 then
+            obj:SetAngles((data.HitNormal + Vector(0, 0, 90)):Angle())
+
+            return
+        end
+
+        if data.HitNormal.x > .5 then
+            obj:SetAngles((data.HitNormal + Vector(0, 0, 90)):Angle())
+
+            return
+        end
+    end
 end
--- lua/entities/arc9_gsr_thrownfrag/shared.lua
+++ lua/entities/arc9_gsr_thrownfrag/shared.lua
@@ -1,86 +1,143 @@
 AddCSLuaFile()
 ENT.Type = "anim"
-ENT.Base = "base_anim"
+ENT.Base = "arc9_nade_base"
 ENT.Spawnable = false
 
+ENT.Model = "models/weapons/w_eq_fraggrenade_thrown.mdl"
+ENT.Damage = 100
+ENT.Radius = 354
+ENT.LifeTime = 1.5
+
+if SERVER then
+    function ENT:Detonate()
+        if not self:IsValid() then return end
+        if self.Defused then return end
+
+        local explode = ents.Create("info_particle_system")
+        explode:SetKeyValue("effect_name", "explosion_hegrenade_brief")
+        explode:SetOwner(self:GetOwner())
+        explode:SetPos(self:GetPos())
+        explode:Spawn()
+        explode:Activate()
+        explode:Fire("start", "", 0)
+        explode:Fire("kill", "", 15)
+        local explode2 = ents.Create("info_particle_system")
+        explode2:SetKeyValue("effect_name", "explosion_hegrenade_interior")
+        explode2:SetOwner(self:GetOwner())
+        explode2:SetPos(self:GetPos())
+        explode2:Spawn()
+        explode2:Activate()
+        explode2:Fire("start", "", 0)
+        explode2:Fire("kill", "", 15)
+        self:EmitSound("CSGO.Frag.Explode")
+        util.BlastDamage(self, self:GetOwner(), self:GetPos(), self.Radius, self.Damage)
+        local spos = self:GetPos()
+
+        local trs = util.TraceLine({
+            start = spos + Vector(0, 0, 64),
+            endpos = spos + Vector(0, 0, -32),
+            filter = self
+        })
+
+        util.Decal("Scorch", trs.HitPos + trs.HitNormal, trs.HitPos - trs.HitNormal)
+
+        self.Defused = true
+        SafeRemoveEntityDelayed(self, self.SmokeTrailTime)
+        self:SetRenderMode(RENDERMODE_NONE)
+        self:SetMoveType(MOVETYPE_NONE)
+        self:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
+    end
+end
+--[[]
 function ENT:Draw()
-	self:DrawModel()
+    self:DrawModel()
 end
 
 function ENT:Initialize()
-	if SERVER then
-		self:SetModel( "models/weapons/w_eq_fraggrenade_thrown.mdl" )
-		self:SetMoveType( MOVETYPE_VPHYSICS )
-		self:SetSolid( SOLID_VPHYSICS )
-		self:PhysicsInit( SOLID_VPHYSICS )
-		self:SetCollisionGroup( COLLISION_GROUP_NONE )
-		self:DrawShadow( false )
-	end
-	self:EmitSound("CSGO.HE.Throw")
-	self.ExplodeTimer = CurTime() + 1.5
+    if SERVER then
+        self:SetModel("models/weapons/w_eq_fraggrenade_thrown.mdl")
+        self:SetMoveType(MOVETYPE_VPHYSICS)
+        self:SetSolid(SOLID_VPHYSICS)
+        self:PhysicsInit(SOLID_VPHYSICS)
+        self:SetCollisionGroup(COLLISION_GROUP_NONE)
+        self:DrawShadow(false)
+    end
+
+    self:EmitSound("CSGO.HE.Throw")
+    self.ExplodeTimer = CurTime() + 1.5
 end
 
 function ENT:Think()
-	if SERVER and self.ExplodeTimer <= CurTime() then
-		self:Explode()
-		self:Remove()
-	end
+    if SERVER and self.ExplodeTimer <= CurTime() then
+        self:Explode()
+        self:Remove()
+    end
 end
 
-function ENT:PhysicsCollide( data )
-	if SERVER and data.Speed > 150 then
-	
-            local tgt = data.HitEntity
-            if IsValid(tgt) and not tgt:IsWorld() and (self.NextHit or 0) < CurTime() then
-                self.NextHit = CurTime() + 0.1
-                local dmginfo = DamageInfo()
-                dmginfo:SetDamageType(DMG_GENERIC)
-                dmginfo:SetDamage(10)
-                dmginfo:SetAttacker(self:GetOwner())
-                dmginfo:SetInflictor(self)
-                dmginfo:SetDamageForce(data.OurOldVelocity * 0.5)
-                tgt:TakeDamageInfo(dmginfo)
-                if (IsValid(tgt) and (tgt:IsNPC() or tgt:IsPlayer() or tgt:IsNextBot()) and tgt:Health() <= 0) or (not tgt:IsWorld() and not IsValid(tgt)) or string.find(tgt:GetClass(), "breakable") then
-                    local pos, ang, vel = self:GetPos(), self:GetAngles(), data.OurOldVelocity
-                    timer.Simple(0, function()
-                        if IsValid(self) then
-                            self:SetAngles(ang)
-                            self:SetPos(pos)
-                            self:GetPhysicsObject():SetVelocityInstantaneous(vel)
-                        end
-                    end)
-                end
+function ENT:PhysicsCollide(data)
+    if SERVER and data.Speed > 150 then
+        local tgt = data.HitEntity
+
+        if IsValid(tgt) and not tgt:IsWorld() and (self.NextHit or 0) < CurTime() then
+            self.NextHit = CurTime() + 0.1
+            local dmginfo = DamageInfo()
+            dmginfo:SetDamageType(DMG_GENERIC)
+            dmginfo:SetDamage(10)
+            dmginfo:SetAttacker(self:GetOwner())
+            dmginfo:SetInflictor(self)
+            dmginfo:SetDamageForce(data.OurOldVelocity * 0.5)
+            tgt:TakeDamageInfo(dmginfo)
+
+            if (IsValid(tgt) and (tgt:IsNPC() or tgt:IsPlayer() or tgt:IsNextBot()) and tgt:Health() <= 0) or (not tgt:IsWorld() and not IsValid(tgt)) or string.find(tgt:GetClass(), "breakable") then
+                local pos, ang, vel = self:GetPos(), self:GetAngles(), data.OurOldVelocity
+
+                timer.Simple(0, function()
+                    if IsValid(self) then
+                        self:SetAngles(ang)
+                        self:SetPos(pos)
+                        self:GetPhysicsObject():SetVelocityInstantaneous(vel)
+                    end
+                end)
             end
-		self:EmitSound( "CSGO.HE.Bounce" )
-	end
+        end
+
+        self:EmitSound("CSGO.HE.Bounce")
+    end
 end
 
 function ENT:OnRemove()
 end
 
 function ENT:Explode()
-if SERVER then
-		local explode = ents.Create( "info_particle_system" )
-		explode:SetKeyValue( "effect_name", "explosion_hegrenade_brief" )
-		explode:SetOwner( self.Owner )
-		explode:SetPos( self:GetPos() )
-		explode:Spawn()
-		explode:Activate()
-		explode:Fire( "start", "", 0 )
-		explode:Fire( "kill", "", 15 )
-		local explode2 = ents.Create( "info_particle_system" )
-		explode2:SetKeyValue( "effect_name", "explosion_hegrenade_interior" )
-		explode2:SetOwner( self.Owner )
-		explode2:SetPos( self:GetPos() )
-		explode2:Spawn()
-		explode2:Activate()
-		explode2:Fire( "start", "", 0 )
-		explode2:Fire( "kill", "", 15 )
-		self:EmitSound( "CSGO.Frag.Explode" )
+    if SERVER then
+        local explode = ents.Create("info_particle_system")
+        explode:SetKeyValue("effect_name", "explosion_hegrenade_brief")
+        explode:SetOwner(self:GetOwner())
+        explode:SetPos(self:GetPos())
+        explode:Spawn()
+        explode:Activate()
+        explode:Fire("start", "", 0)
+        explode:Fire("kill", "", 15)
+        local explode2 = ents.Create("info_particle_system")
+        explode2:SetKeyValue("effect_name", "explosion_hegrenade_interior")
+        explode2:SetOwner(self:GetOwner())
+        explode2:SetPos(self:GetPos())
+        explode2:Spawn()
+        explode2:Activate()
+        explode2:Fire("start", "", 0)
+        explode2:Fire("kill", "", 15)
+        self:EmitSound("CSGO.Frag.Explode")
+    end
+
+    util.BlastDamage(self, self:GetOwner(), self:GetPos(), 354, 98)
+    local spos = self:GetPos()
+
+    local trs = util.TraceLine({
+        start = spos + Vector(0, 0, 64),
+        endpos = spos + Vector(0, 0, -32),
+        filter = self
+    })
+
+    util.Decal("Scorch", trs.HitPos + trs.HitNormal, trs.HitPos - trs.HitNormal)
 end
-	util.BlastDamage( self, self.Owner, self:GetPos(), 354, 98 )
-	
-	local spos = self:GetPos()
-	local trs = util.TraceLine({start=spos + Vector(0,0,64), endpos=spos + Vector(0,0,-32), filter=self})
-	util.Decal("Scorch", trs.HitPos + trs.HitNormal, trs.HitPos - trs.HitNormal)    
-end
+]]
