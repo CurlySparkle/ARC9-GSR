5f2d523d1f11b9545608f0c4a3809357210745ce
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2022-12-24 14:52:22 +0700

    bilingual city my ass, Brussels is 90% francophone

--- lua/arc9/common/attachments/go_grip_gripod.lua
+++ lua/arc9/common/attachments/go_grip_gripod.lua
@@ -5,6 +5,7 @@ ATT.Description = [[Mounted foregrip with bipod functionality. Provides the most
 ATT.Icon = Material("entities/attachs/grip_gripod.png", "mips smooth")
 
 ATT.Model = "models/weapons/csgo/atts/grip_gripod.mdl"
+ATT.ModelBodygroups = "00000"
 
 ATT.SortOrder = 0
 ATT.Category = "grip"
@@ -22,3 +23,13 @@ ATT.RecoilMult = 0.9
 ATT.RecoilMultCrouch = 0.75
 ATT.RecoilPatternDriftMult = 0.75
 ATT.RecoilAutoControlMult = 1.15
+
+ATT.Bipod = true
+
+ATT.DrawFunc = function(self, model, wm)
+	if self:GetBipod() then
+		model:SetBodygroup(0,1)
+	end
+end
+ATT.BipodPos = Vector(-6, 0, 0)
+ATT.BipodAng = Angle(0, 0, 0)
