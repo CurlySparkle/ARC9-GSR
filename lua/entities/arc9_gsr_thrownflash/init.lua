f74f34033df78bdeb3acc679f449d0541c5b9ad5
TheOnly8Z <888888zombies@gmail.com>
2023-01-03 23:59:57 -0600

    flashbang npc interaction

--- lua/entities/arc9_gsr_thrownflash/init.lua
+++ lua/entities/arc9_gsr_thrownflash/init.lua
@@ -1,150 +1,138 @@
 -- Made by Matsilagi
-
-AddCSLuaFile( "cl_init.lua" )
-AddCSLuaFile( "shared.lua" )
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
 include('shared.lua')
 
 function ENT:Initialize()
-	self:SetModel("models/weapons/w_eq_flashbang_thrown.mdl")
+    self:SetModel("models/weapons/w_eq_flashbang_thrown.mdl")
+    self:PhysicsInit(SOLID_VPHYSICS)
+    --self:PhysicsInitSphere( ( self:OBBMaxs() - self:OBBMins() ):Length()/4, "metal" )
+    self:SetMoveType(MOVETYPE_VPHYSICS)
+    self:SetSolid(SOLID_VPHYSICS)
+    self:DrawShadow(false)
+    self:SetCollisionGroup(COLLISION_GROUP_NONE)
+    self:EmitSound("CSGO.HE.Throw")
+    self.timeleft = CurTime() + 2 -- HOW LONG BEFORE EXPLOSION
 
-	self:PhysicsInit(SOLID_VPHYSICS)
-	--self:PhysicsInitSphere( ( self:OBBMaxs() - self:OBBMins() ):Length()/4, "metal" )
-	self:SetMoveType( MOVETYPE_VPHYSICS )
-	self:SetSolid( SOLID_VPHYSICS )
-	self:DrawShadow( false )
-	self:SetCollisionGroup( COLLISION_GROUP_NONE )
+    if SERVER then
+        timer.Simple(30, function()
+            if IsValid(self) then
+                self:Remove()
+            end
+        end)
+    end
 
-	self:EmitSound("CSGO.HE.Throw")
-
-	self.timeleft = CurTime() + 2 -- HOW LONG BEFORE EXPLOSION
-
-	if SERVER then
-		timer.Simple(30,function()
-			if IsValid(self) then
-				self:Remove()
-			end
-		end)
-	end
-
-	self:Think()
+    self:Think()
 end
 
- function ENT:Think()
+function ENT:Think()
+    if self.timeleft < CurTime() and not self.deactivated then
+        self:Explosion()
+        self:Remove()
+    end
 
-	if self.timeleft < CurTime() and !self.deactivated then
-		self:Explosion()
-	    self:Remove()
-	end
+    self:NextThink(CurTime())
 
-	self:NextThink( CurTime() )
-	return true
+    return true
 end
 
-function ENT:EntityFacingFactor( theirent )
-	local dir = theirent:EyeAngles():Forward()
-	local facingdir = (self:GetPos() - (theirent.GetShootPos and theirent:GetShootPos() or theirent:GetPos())):GetNormalized()
-	return (facingdir:Dot(dir)+1)/2
+function ENT:EntityFacingFactor(theirent)
+    local dir = theirent:EyeAngles():Forward()
+    local facingdir = (self:GetPos() - (theirent.GetShootPos and theirent:GetShootPos() or theirent:GetPos())):GetNormalized()
+
+    return (facingdir:Dot(dir) + 1) / 2
 end
 
-function ENT:EntityFacingUs( theirent )
-	local dir = theirent:EyeAngles():Forward()
-	local facingdir = (self:GetPos()-(theirent.GetShootPos and theirent:GetShootPos() or theirent:GetPos())):GetNormalized()
-	if facingdir:Dot(dir)>-0.25 then return true end
+function ENT:EntityFacingUs(theirent)
+    local dir = theirent:EyeAngles():Forward()
+    local facingdir = (self:GetPos() - (theirent.GetShootPos and theirent:GetShootPos() or theirent:GetPos())):GetNormalized()
+    if facingdir:Dot(dir) > -0.25 then return true end
 end
-
 
 function ENT:Explosion()
-	local tr = {}
-	tr.start = self:GetPos()
-	tr.mask = MASK_SOLID
+    local tr = {}
+    tr.start = self:GetPos()
+    tr.mask = MASK_SOLID
 
-	for _, v in ipairs(player.GetAll()) do
-		tr.endpos = v:GetShootPos()
-		tr.filter = { self, v, v:GetActiveWeapon() }
-		local traceres = util.TraceLine(tr)
+    for _, v in ipairs(player.GetAll()) do
+        tr.endpos = v:GetShootPos()
 
-		if !traceres.Hit or traceres.Fraction>=1 or traceres.Fraction<=0 then
-			v:SetNWFloat("ARC9_GSR_LastFlash", CurTime())
-			v:SetNWEntity("ARC9_GSR_LastFlashBy", self:GetOwner())
-			v:SetNWFloat("ARC9_GSR_FlashDistance", v:GetShootPos():Distance(self:GetPos()))
-			v:SetNWFloat("ARC9_GSR_FlashFactor", self:EntityFacingFactor(v))
+        tr.filter = {self, v, v:GetActiveWeapon()}
 
-			if v:GetNWFloat("ARC9_GSR_FlashDistance",v:GetShootPos():Distance(self:GetPos())) < 1500 and v:GetNWFloat("FlashFactor",self:EntityFacingFactor(v)) < tr.endpos:Distance(self:GetPos(v)) then
-				if v:GetNWFloat("ARC9_GSR_FlashDistance",v:GetShootPos():Distance(self:GetPos())) < 1000 then
-					v:SetDSP( 37 , false )
-				elseif v:GetNWFloat("ARC9_GSR_FlashDistance",v:GetShootPos():Distance(self:GetPos())) < 800 then
-					v:SetDSP( 36 , false )
-				elseif v:GetNWFloat("ARC9_GSR_FlashDistance",v:GetShootPos():Distance(self:GetPos())) < 600 then
-					v:SetDSP( 35, false )
-				end
-			end
-		end
-	end
-	
+        local traceres = util.TraceLine(tr)
+
+        if not traceres.Hit or traceres.Fraction >= 1 or traceres.Fraction <= 0 then
+            v:SetNWFloat("ARC9_GSR_LastFlash", CurTime())
+            v:SetNWEntity("ARC9_GSR_LastFlashBy", self:GetOwner())
+            v:SetNWFloat("ARC9_GSR_FlashDistance", v:GetShootPos():Distance(self:GetPos()))
+            v:SetNWFloat("ARC9_GSR_FlashFactor", self:EntityFacingFactor(v))
+
+            if v:GetNWFloat("ARC9_GSR_FlashDistance", v:GetShootPos():Distance(self:GetPos())) < 1500 and v:GetNWFloat("FlashFactor", self:EntityFacingFactor(v)) < tr.endpos:Distance(self:GetPos(v)) then
+                if v:GetNWFloat("ARC9_GSR_FlashDistance", v:GetShootPos():Distance(self:GetPos())) < 1000 then
+                    v:SetDSP(37, false)
+                elseif v:GetNWFloat("ARC9_GSR_FlashDistance", v:GetShootPos():Distance(self:GetPos())) < 800 then
+                    v:SetDSP(36, false)
+                elseif v:GetNWFloat("ARC9_GSR_FlashDistance", v:GetShootPos():Distance(self:GetPos())) < 600 then
+                    v:SetDSP(35, false)
+                end
+            end
+        end
+    end
+
     if self:WaterLevel() > 0 then
         local tr = util.TraceLine({
             start = self:GetPos(),
             endpos = self:GetPos() + Vector(0, 0, 1) * 1024,
             filter = self,
         })
+
         local tr2 = util.TraceLine({
             start = tr.HitPos,
             endpos = self:GetPos(),
             filter = self,
             mask = MASK_WATER
         })
-        ParticleEffect("explosion_water", tr2.HitPos + Vector(0, 0, 8), Angle(0, 0, 0), nil)
 
+        ParticleEffect("explosion_water", tr2.HitPos + Vector(0, 0, 8), Angle(0, 0, 0), nil)
         self:EmitSound("weapons/underwater_explode3.wav", 100)
     else
         ParticleEffect("bumpmine_detonate", self:GetPos(), Angle(0, 0, 0), nil)
-		ParticleEffect("weapon_decoy_ground_effect_shot", self:GetPos(), Angle(0, 0, 0), nil)
-		ParticleEffect("explosion_hegrenade_brief", self:GetPos(), Angle(0, 0, 0), nil)
+        ParticleEffect("weapon_decoy_ground_effect_shot", self:GetPos(), Angle(0, 0, 0), nil)
+        ParticleEffect("explosion_hegrenade_brief", self:GetPos(), Angle(0, 0, 0), nil)
         self:EmitSound("CSGO.Flashbang.Explode")
     end
 
-	for _, v in ipairs(ents.GetAll()) do
-		if v:IsNPC() and self:EntityFacingUs(v) then
-			tr.endpos = v.GetShootPos and v:GetShootPos() or v:GetPos()
-			tr.filter = { self, v, v.GetActiveWeapon and v:GetActiveWeapon() or v}
-			local traceres = util.TraceLine(tr)
-			if !traceres.Hit or traceres.Fraction>=1 or traceres.Fraction<=0 then
+    for _, v in ipairs(ents.GetAll()) do
+        if v:IsNPC() and self:EntityFacingUs(v) then
+            tr.endpos = v.GetShootPos and v:GetShootPos() or v:GetPos()
 
-				local flashdistance = tr.endpos:Distance(self:GetPos())
-				local flashtime = CurTime()
+            tr.filter = {self, v, v.GetActiveWeapon and v:GetActiveWeapon() or v}
 
-				local distancefac = ( 1-math.Clamp((flashdistance-gsr_flashdistance+gsr_flashdistancefade)/gsr_flashdistancefade,0,1) )
-				local intensity = ( 1-math.Clamp(((CurTime()-flashtime)/distancefac-gsr_flashtime+gsr_flashfade)/gsr_flashfade,0,1) )
+            local traceres = util.TraceLine(tr)
 
-				if intensity>0.8 then
-					v:SetNWFloat("ARC9_GSR_LastFlash", CurTime())
-					v:SetNWEntity("ARC9_GSR_LastFlashBy", self:GetOwner())
-					v:SetNWFloat("ARC9_GSR_FlashDistance", v:GetShootPos():Distance(self:GetPos()))
-					v:SetNWFloat("ARC9_GSR_FlashFactor", self:EntityFacingFactor(v))
+            if not traceres.Hit or traceres.Fraction >= 1 or traceres.Fraction <= 0 then
+                local flashdistance = tr.endpos:Distance(self:GetPos())
+                local flashtime = CurTime()
+                local distancefac = 1 - math.Clamp((flashdistance - gsr_flashdistance + gsr_flashdistancefade) / gsr_flashdistancefade, 0, 1)
+                local intensity = 1 - math.Clamp(((CurTime() - flashtime) / distancefac - gsr_flashtime + gsr_flashfade) / gsr_flashfade, 0, 1)
 
-					if v.ClearSchedule then
-						v:ClearSchedule()
-					end
-					if v.SetEnemy then
-						v:SetEnemy(nil)
-					end
-					if v.AddEntityRelationship and IsValid(self.Owner) then
-						local oldrel = v.GetRelationship and v:GetRelationship(self.Owner) or ( ( IsFriendEntityName( v:GetClass() ) and !game.GetMap()=="gm_raid" ) and D_LI or D_HT )
-						v:AddEntityRelationship( self.Owner, D_NU, 99)
-						timer.Simple(gsr_flashtime/2, function()
-							if IsValid(v) and v:IsNPC() and IsValid(self) and IsValid(self.Owner) then
-								v:AddEntityRelationship( self.Owner, oldrel, 99)
-							end
-						end)
-					end
-					if v.ClearEnemyMemory then
-						v:ClearEnemyMemory()
-					end
-				end
-			end
-		end
-	end
-	
+                if intensity > 0.2 then
+                    v:SetEnemy(nil)
+                    v:SetNPCState(NPC_STATE_PLAYDEAD)
+
+                    timer.Simple(intensity * gsr_flashtime * 2, function()
+                        if !IsValid(v) then return end
+                        v:SetNPCState(NPC_STATE_IDLE)
+                    end)
+
+                    if v.ClearEnemyMemory then
+                        v:ClearEnemyMemory()
+                    end
+                end
+            end
+        end
+    end
+
     if SERVER then
         local dir = self.HitVelocity or self:GetVelocity()
 
@@ -163,44 +151,39 @@ function ENT:Explosion()
         })
     end
 
-	local dlight = EffectData()
-	dlight:SetOrigin(self:GetPos())
-	dlight:SetEntity(self.Owner) //i dunno, just use it!
-	util.Effect("arc9_flashbang_light", dlight)
-	
-	self.deactivated = true
+    local dlight = EffectData()
+    dlight:SetOrigin(self:GetPos())
+    dlight:SetEntity(self.Owner) --i dunno, just use it!
+    util.Effect("arc9_flashbang_light", dlight)
+    self.deactivated = true
 end
 
-/*---------------------------------------------------------
+--[[---------------------------------------------------------
 OnTakeDamage
----------------------------------------------------------*/
-function ENT:OnTakeDamage( dmginfo )
+---------------------------------------------------------]]
+function ENT:OnTakeDamage(dmginfo)
 end
 
-
-/*---------------------------------------------------------
+--[[---------------------------------------------------------
 Use
----------------------------------------------------------*/
-function ENT:Use( activator, caller, type, value )
+---------------------------------------------------------]]
+function ENT:Use(activator, caller, type, value)
 end
 
-
-/*---------------------------------------------------------
+--[[---------------------------------------------------------
 StartTouch
----------------------------------------------------------*/
-function ENT:StartTouch( entity )
+---------------------------------------------------------]]
+function ENT:StartTouch(entity)
 end
 
-
-/*---------------------------------------------------------
+--[[---------------------------------------------------------
 EndTouch
----------------------------------------------------------*/
-function ENT:EndTouch( entity )
+---------------------------------------------------------]]
+function ENT:EndTouch(entity)
 end
 
-
-/*---------------------------------------------------------
+--[[---------------------------------------------------------
 Touch
----------------------------------------------------------*/
-function ENT:Touch( entity )
+---------------------------------------------------------]]
+function ENT:Touch(entity)
 end
