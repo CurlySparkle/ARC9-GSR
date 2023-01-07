edf9473b67b6ab6b6b21f1eae649dec7e01901a5
CturiX <52268297+CturiX@users.noreply.github.com>
2023-01-08 02:47:56 +1300

    reload sound scripts
    for more complex handling sfx

---
+++ lua/arc9/shared/sh_csgo_sounds_reloads.lua
@@ -0,0 +1,92 @@
+AddCSLuaFile()
+
+sound.Add( {
+    name = "ARC9_UNIVERSAL.ShellDrop",
+    channel = CHAN_STATIC,
+    volume = {0.3, 0.3},
+    level = 75,
+    pitch = {20, 150},
+    sound = {
+    "player/pl_shell1.wav",
+    "player/pl_shell2.wav",
+    "player/pl_shell3.wav"
+    }
+} )
+
+
+
+
+
+
+sound.Add( {
+    name = "ARC9_CSGO_HKP2000.Magin",
+    channel = CHAN_STATIC,
+    volume = {0.7, 0.9},
+    level = 75,
+    pitch = {95, 103},
+    sound = {
+        "weapons/csgo/hkp2000/hkp2000_in1.wav",
+        "weapons/csgo/hkp2000/hkp2000_in2.wav",
+    }
+} )
+sound.Add( {
+    name = "ARC9_CSGO_HKP2000.Magout",
+    channel = CHAN_STATIC,
+    volume = {0.7, 0.9},
+    level = 75,
+    pitch = {95, 103},
+    sound = {
+        "weapons/csgo/hkp2000/hkp2000_out1.wav",
+        "weapons/csgo/hkp2000/hkp2000_out2.wav",
+    }
+} )
+sound.Add( {
+    name = "ARC9_CSGO_HKP2000.MagoutEmpty",
+    channel = CHAN_STATIC,
+    volume = {0.6, 0.7},
+    level = 75,
+    pitch = {95, 103},
+    sound = {
+        "weapons/csgo/hkp2000/hkp2000_out_empty.wav",
+    }
+} )
+sound.Add( {
+    name = "ARC9_CSGO_HKP2000.ReloadEnd",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 75,
+    pitch = {95, 103},
+    sound = {
+        "weapons/csgo/hkp2000/hkp2000_reload_end.wav",
+    }
+} )
+sound.Add( {
+    name = "ARC9_CSGO_HKP2000.Slideback",
+    channel = CHAN_STATIC,
+    volume = {0.7, 0.9},
+    level = 75,
+    pitch = {95, 103},
+    sound = {
+        "weapons/csgo/hkp2000/hkp2000_back.wav",
+    }
+} )
+sound.Add( {
+    name = "ARC9_CSGO_HKP2000.Slideforward",
+    channel = CHAN_STATIC,
+    volume = {0.7, 0.9},
+    level = 75,
+    pitch = {95, 103},
+    sound = {
+        "weapons/csgo/hkp2000/hkp2000_forward.wav",
+    }
+} )
+sound.Add( {
+    name = "ARC9_CSGO_HKP2000.SlideforwardAlt",
+    channel = CHAN_STATIC,
+    volume = {0.7, 0.9},
+    level = 75,
+    pitch = {95, 103},
+    sound = {
+        "weapons/csgo/hkp2000/hkp2000_forward_alt.wav",
+    }
+} )
