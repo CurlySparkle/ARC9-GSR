90f821b72dc630860b95ddd414ee09ffb1a9ffa8
May Lian <twilight_sparkle13@hotmail.com>
2023-01-27 18:30:47 -0300

    Added claymore

---
+++ lua/entities/arc9_gsr_thrownclaymore/shared.lua
@@ -0,0 +1,200 @@
+ENT.Type = "anim"
+ENT.Base = "base_entity"
+ENT.PrintName = "Claymore"
+ENT.Author = ""
+ENT.Information = ""
+ENT.Spawnable = false
+ENT.AdminSpawnable = false
+
+ENT.Model = "models/weapons/w_eq_claymore_dropped.mdl"
+ENT.FuseTime = 9999
+ENT.ArmTime = 0
+ENT.ImpactFuse = false
+
+ENT.Armed = false
+ENT.NextBeepTime = 0
+ENT.BeepPitch = 100
+
+AddCSLuaFile()
+
+function ENT:Initialize()
+    if SERVER then
+        self:SetModel( self.Model )
+        self:SetMoveType( MOVETYPE_VPHYSICS )
+        self:SetSolid( SOLID_VPHYSICS )
+        self:PhysicsInit( SOLID_VPHYSICS )
+        self:DrawShadow( true )
+
+        local phys = self:GetPhysicsObject()
+        if phys:IsValid() then
+            phys:Wake()
+            phys:SetBuoyancyRatio(0)
+        end
+
+        self:SetHealth(5)
+        self:SetMaxHealth(5)
+
+        self.Owner = self:GetOwner()
+        self:SetOwner(NULL)
+
+        self.SpawnTime = CurTime()
+        self.TheAngle = self:GetAngles()
+        self.FuseTime = 999999
+
+        timer.Simple(0, function()
+            if !IsValid(self) then return end
+            self:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
+        end)
+    end
+end
+
+function ENT:PhysicsCollide(data, physobj)
+    if SERVER then
+        if data.HitEntity:IsWorld() then
+            --self:EmitSound("weapons/csgo/claymore/claymore_plant_01.wav")
+			self:EmitSound(Sound("weapons/csgo/claymore/claymore_plant_0" .. math.random(1,3) .. ".wav"))
+
+            local ang = self:GetAngles()
+
+            if self.TheAngle then
+                ang = self.TheAngle
+            end
+
+            ang.p = 0
+            ang.r = 0
+
+            self:SetAngles(ang)
+
+            self:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
+            self:SetPos(data.HitPos)
+            self:SetMoveType(MOVETYPE_NONE)
+            self:SetPos(data.HitPos - (data.HitNormal * 2))
+
+            self.Armed = true
+        end
+
+    end
+end
+
+function ENT:Use(act, call, calltype, integer)
+    if act:IsPlayer() then
+        if weapons.GetStored("arc9_go_nade_claymores").Primary.Ammo ~= "" then
+            act:GiveAmmo(1, weapons.GetStored("arc9_go_nade_claymores").Primary.Ammo)
+        end
+        act:Give("arc9_go_nade_claymores", true)
+    end
+
+    self:EmitSound("weapons/csgo/bumpmines/bumpmine_pickup.wav", 75)
+    self:Remove()
+end
+
+function ENT:Think()
+    if SERVER and self.FuseTime >= 0 and CurTime() - self.SpawnTime >= self.FuseTime then
+        self:Detonate()
+    end
+
+    if SERVER and self.Armed then
+        local e = ents.FindInCone(self:GetPos(), self:GetAngles():Forward(), 200, 0.8)
+
+        for _, i in pairs(e) do
+            if !IsValid(i) then continue end
+
+            if i:IsPlayer() or i:IsNPC() or i:IsNextBot() then
+                self:Detonate()
+                break
+            end
+        end
+    end
+
+    if CLIENT then
+        if self.NextBeepTime <= CurTime() then
+            self:EmitSound("weapons/csgo/claymore/claymore_sensors_on.wav", 75, self.BeepPitch)
+
+            self.NextBeepTime = CurTime() + 5
+        end
+    end
+end
+
+ENT.AntiRecurse = false
+
+function ENT:OnTakeDamage(dmg)
+    if self.AntiRecurse then return end
+
+    self.AntiRecurse = true
+    self:Detonate()
+end
+
+function ENT:Detonate()
+    if SERVER then
+        if !self:IsValid() then return end
+        local effectdata = EffectData()
+            effectdata:SetOrigin( self:GetPos() )
+
+        if self:WaterLevel() >= 1 then
+            util.Effect( "WaterSurfaceExplosion", effectdata )
+            self:EmitSound("weapons/underwater_explode3.wav", 120, 100, 1, CHAN_AUTO)
+        else
+		    ParticleEffect("explosion_hegrenade_brief", self:GetPos(), Angle(0, 0, 0), nil)
+		    ParticleEffect("explosion_hegrenade_interior", self:GetPos(), Angle(0, 0, 0), nil)
+            ParticleEffect("grenade_explosion_01", self:GetPos(), self:GetAngles(), nil)
+			ParticleEffect("weapon_decoy_ground_effect_shot", self:GetPos(), Angle(0, 0, 0), nil)
+		    ParticleEffect("smoke_plume_b", self:GetPos(), Angle(0, 0, 0), nil)
+		    ParticleEffect("smoke_plume_c", self:GetPos(), Angle(0, 0, 0), nil)
+		    ParticleEffect("HE_shockwave", self:GetPos(), Angle(0, 0, 0), nil)
+			
+            --util.Effect("HelicopterMegaBomb", fx)
+		    local spos = self:GetPos()
+		    local trs = util.TraceLine({start=spos + Vector(0,0,64), endpos=spos + Vector(0,0,-32), filter=self})
+		    util.Decal("Scorch", trs.HitPos + trs.HitNormal, trs.HitPos - trs.HitNormal)
+            self:EmitSound("weapons/csgo/claymore/claymore_expl_01.wav", 125, 100, 1, CHAN_AUTO)
+        end
+
+        local oldowner = self:GetOwner() or self.Owner
+
+        if !IsValid(oldowner) then
+            oldowner = self
+        end
+
+        self:SetOwner(nil)
+
+        -- util.BlastDamage(oldowner, oldowner, self:GetPos(), 200, 25)
+
+        local btabl = {
+            Attacker = oldowner,
+            Damage = 35,
+            Distance = 500,
+            Num = 40,
+            Dir = (self:GetAngles() + Angle(-5, 0, 0)):Forward(),
+            Src = self:GetPos() + Vector(0, 0, 3),
+            Spread = Vector(0.8, 0.17, 0),
+            Callback = function(att, tr, dmg)
+                dmg:SetDamageType(DMG_BLAST)
+                if IsValid(oldowner) then
+                    dmg:SetAttacker(oldowner)
+                end
+            end
+        }
+
+        self:FireBullets(btabl)
+
+        self:Remove()
+    end
+end
+
+function ENT:Draw()
+    if CLIENT then
+        self:DrawModel()
+
+        local pos = self:GetPos()
+
+        pos = pos + Vector(0, 0, 6)
+
+        if self.NextBeepTime - 4.9 >= CurTime() and self.NextBeepTime - 5 <= CurTime() then
+		    --self:EmitSound("weapons/csgo/claymore/claymore_sensors_on.wav", 75)
+            cam.Start3D() -- Start the 3D function so we can draw onto the screen.
+                render.SetMaterial( Material("effects/blueflare1") ) -- Tell render what material we want, in this case the flash from the gravgun
+                render.DrawSprite( pos, 25, 25, Color(255, 0, 0) ) -- Draw the sprite in the middle of the map, at 16x16 in it's original colour with full alpha.
+            cam.End3D()
+        end
+    end
+end
---
+++ lua/weapons/arc9_go_nade_claymores.lua
@@ -0,0 +1,183 @@
+SWEP.CustomSelectIcon = Material("vgui/hud/arc9_go_nade_mines")
+
+SWEP.Base = "arc9_base_nade"
+SWEP.Category = "ARC9 - GS:R"
+SWEP.SubCategory = "Extras"
+SWEP.Spawnable = true
+SWEP.AdminOnly = false
+SWEP.NoDynamicKillIcon = true
+SWEP.NotForNPCs = true
+
+SWEP.PrintName = "Claymores"
+SWEP.Class = "Directional Mine"
+SWEP.Description = [[Toss one of these on the ground and send your opponents flying (also can be set to explode). Hope you don't step on one.]]
+SWEP.Trivia = {
+    Designer = "Norman Macleod et al.",
+    Manufacturer = "Mohawk Electrical Systems Inc",
+    Mechanism = "Detonation by Radius",
+    Origin = "United States of America",
+    ["Explosive Mass"] = "680g",
+    ["Explosive Type"] = "C4"
+}
+
+SWEP.Credits = {
+    Author = "Twilight Sparkle/SlogoKolt/Arctic/TheOnly8Z/Matsilagi/CturiX",
+    Assets = "Counter-Strike: Global Offensive/COD: Modern Warfare 2019"
+}
+
+SWEP.Slot = 4
+
+SWEP.Firemodes = {
+    {
+        Mode = 1,
+        PrintName = "PLANT"
+        -- add other attachment modifiers
+    },
+}
+
+SWEP.UseHands = true
+
+SWEP.ViewModel = "models/weapons/csgo/c_eq_claymores.mdl"
+SWEP.WorldModel = "models/weapons/w_eq_claymore.mdl"
+SWEP.MirrorVMWM = false
+SWEP.WorldModelOffset = {
+    Pos        =    Vector(-3, 5, -7.5),
+    Ang        =    Angle(5, 7, 180),
+    Bone    =    "ValveBiped.Bip01_R_Hand",
+    TPIKPos = Vector(-7, 1, -0),
+    TPIKAng = Angle(0, 0, 180),
+    Scale = 1,
+}
+
+SWEP.BottomlessClip = true
+SWEP.ClipSize = 3
+SWEP.ChamberSize = 0
+SWEP.SupplyLimit = 0
+SWEP.Crosshair = true
+
+SWEP.FreeAimRadius = 0 -- In degrees, how much this gun can free aim in hip fire.
+SWEP.Sway = 0 -- How much the gun sways.
+
+SWEP.ShootEnt = "arc9_gsr_thrownclaymore"
+SWEP.Ammo = "slam" -- What ammo type this gun uses.
+
+SWEP.Throwable = true -- Set to true to give this weapon throwing capabilities.
+SWEP.Tossable = false -- When grenade is enabled, right click will toss. Set to false to disable, allowing you to aim down sights.
+SWEP.ThrowAnimSpeed = 1
+
+SWEP.FuseTimer = -1 -- Length of time that the grenade will take to explode in your hands. -1 = Won't explode.
+
+SWEP.ThrowForceMin = 0 -- Minimum force that the grenade will be thrown with.
+SWEP.ThrowForceMax = 0 -- Maximum force that the grenade will be thrown with.
+SWEP.TossForce = 0 -- Force that the grenade will be thrown with when right clicked.
+
+SWEP.ThrowChargeTime = 0 -- How long it takes to charge the grenade to its maximum throw force.
+
+SWEP.Disposable = true 
+
+SWEP.ThrowTumble = false
+
+SWEP.ThrowInstantly = true
+
+SWEP.ShootPosOffset = Vector(0, 0, -15)
+
+-------------------------- POSITIONS
+
+SWEP.HasSights = false
+
+SWEP.ViewModelFOVBase = 65
+
+SWEP.SprintPos = Vector(0, -1, -2)
+SWEP.SprintAng = Angle(-35, -5, 0)
+
+SWEP.SprintMidPoint = {
+    Pos = Vector(0, -1.5, -0.15),
+    Ang = Angle(0, 0, 0)
+}
+
+SWEP.ActivePos = Vector(0, 0, 0)
+SWEP.ActiveAng = Angle(0, 0, 0)
+
+SWEP.MovingMidPoint = {
+    Pos = Vector(0, -0.5, -0.5),
+    Ang = Angle(0, 0, 0)
+}
+
+SWEP.MovingPos = Vector(0, -1, -1)
+SWEP.MovingAng = Angle(0, 0, 0)
+
+SWEP.CrouchPos = Vector(-0.5, -0, -1)
+SWEP.CrouchAng = Angle(0, 0, 0)
+
+SWEP.CustomizeAng = Angle(90, 0, 0)
+SWEP.CustomizePos = Vector(18, 20, 3)
+
+SWEP.CustomizeSnapshotAng = Angle(90, 0, 0)
+SWEP.CustomizeSnapshotPos = Vector(-13, 10, 3)
+SWEP.CustomizeSnapshotFOV = 90
+SWEP.CustomizeNoRotate = false
+
+-------------------------- HoldTypes
+
+SWEP.HoldType = "slam"
+SWEP.HoldTypeSprint = "slam"
+SWEP.HoldTypeHolstered = "slam"
+SWEP.HoldTypeSights = "slam"
+SWEP.HoldTypeCustomize = "slam"
+SWEP.HoldTypeBlindfire = "pistol"
+
+SWEP.AnimShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
+SWEP.AnimReload = ACT_HL2MP_GESTURE_RELOAD_MAGIC
+SWEP.AnimDraw = false
+
+SWEP.CamQCA = 1
+SWEP.CamQCA_Mult = 0.5
+
+function SWEP:SecondaryAttack()
+    return self:MeleeAttack()
+end
+
+SWEP.BashDamage = 35
+SWEP.BashLungeRange = 0
+SWEP.BashRange = 64
+SWEP.PreBashTime = 0.2
+SWEP.PostBashTime = 0.5
+
+SWEP.ImpactForce = 15
+
+SWEP.MeleeHitSound = "CSGO.Melee.HitBody"
+SWEP.MeleeHitWallSound = "CSGO.Melee.HitWall"
+SWEP.MeleeSwingSound = "CSGO.Shield.Swing"
+
+SWEP.Animations = {
+    ["idle"] = {
+        Source = "idle",
+    },
+    -- ["idle_primed"] = {
+        -- Source = "idle_primed"
+    -- },
+    ["draw"] = {
+        Source = "deploy",
+        EventTable = {
+            {s = "CSGO.Item.Movement", t = 0 / 30},
+        },
+    },
+    ["holster"] = {
+        Source = "holster",
+        EventTable = {
+            {s = "CSGO.Item.Movement", t = 0 / 30},
+        },
+    },
+    ["throw"] = {
+        Source = "throw",
+        EventTable = {
+            { s = "weapons/csgo/claymore/claymore_pullback.wav", t = 0 / 30},
+			{ s = "weapons/csgo/claymore/claymore_putdown.wav", t = 6 / 30},
+			{ s = "weapons/csgo/claymore/claymore_plant_03.wav", t = 10 / 30},
+        },
+        MinProgress = 0.3
+    },
+    ["bash"] = {
+        Source = {"melee","melee2","melee3"}
+    },
+}
