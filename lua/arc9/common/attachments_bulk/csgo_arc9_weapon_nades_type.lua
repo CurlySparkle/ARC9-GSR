ecae1c34297022a4858da54b6c91844c375da0f6
Firmeteran <mfirmansyah12345@gmail.com>
2023-02-10 12:50:29 +0700

    Update csgo_arc9_weapon_nades_type.lua
    Missing icon fix.

--- lua/arc9/common/attachments_bulk/csgo_arc9_weapon_nades_type.lua
+++ lua/arc9/common/attachments_bulk/csgo_arc9_weapon_nades_type.lua
@@ -5,7 +5,7 @@ ATT = {}
 ATT.MenuCategory = "ARC9 - CSGO Attachments"
 ATT.PrintName = [[Nade Fuse]]
 ATT.Description = [[Makes your nade have a fuse for 3 seconds (Better plan your throws).]]
-ATT.Icon = Material("entities/attachs/go_nade_timer")
+ATT.Icon = Material("entities/attachs/go_nade_timer.png", "mips smooth")
 
 ATT.SortOrder = 0
 ATT.Free = true
