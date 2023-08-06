d90eff365c3e4a701720c787e059e847f656b548
grande1900 <jlyric2015@gmail.com>
2023-08-06 18:14:19 -0400

    Increase G3SG1 and Scout penetration

--- lua/weapons/arc9_go_g1sg3.lua
+++ lua/weapons/arc9_go_g1sg3.lua
@@ -57,7 +57,7 @@ SWEP.DamageRand = 0.1 -- Damage varies randomly per shot by this fraction. 0.1 =
 SWEP.RangeMin = 3000 -- How far bullets retain their maximum damage for.
 SWEP.RangeMax = 8000 -- In Hammer units, how far bullets can travel before dealing DamageMin.
 
-SWEP.Penetration = 15 -- Units of wood that can be penetrated by this gun.
+SWEP.Penetration = 25 -- Units of wood that can be penetrated by this gun.
 
 SWEP.ImpactForce = 8
 
